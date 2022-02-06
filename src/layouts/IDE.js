33126868db7ec5da655ab327e35d066898756a53
Gökhan Demirtaş <gokhan.demirtas@msn.com>
2022-02-06 16:13:49 +0300

    Refactor styles in IDE

--- src/layouts/IDE.js
+++ src/layouts/IDE.js
@@ -12,47 +12,18 @@ import SettingsEthernetIcon from "@mui/icons-material/SettingsEthernet";
 import StorageIcon from "@mui/icons-material/Storage";
 import ViewCarouselIcon from "@mui/icons-material/ViewCarousel";
 import ViewListIcon from "@mui/icons-material/ViewList";
-import makeStyles from "@mui/styles/makeStyles";
 import { useContext } from "../context";
 import { v4 as uuid } from "uuid";
-import { Box, Drawer, ListItem } from "@mui/material";
+import { Box, Drawer, Grid, ListItem } from "@mui/material";
 import React, { useState } from "react";
 
-const useStyles = makeStyles((theme) => {
-  const ratio = 0.6;
-  const height = 350;
+const ratio = 0.6;
+const height = 350;
 
-  return {
-    root: {
-      display: "flex",
-    },
-    panel: {
-      flex: 1,
-      display: "flex",
-      flexDirection: "column",
-    },
-    main: {
-      flex: 1,
-      padding: theme.spacing(1),
-    },
-    drawer: {
-      top: (window.innerHeight * ratio) / 2 - height / 2 + theme.spacing(1),
-      height,
-      borderTopLeftRadius: "5px",
-      borderBottomLeftRadius: "5px",
-      background: "#353e48",
-      display: "flex",
-      flexDirection: "column",
-      justifyContent: "space-between",
-      paddingTop: theme.spacing(1),
-      paddingBottom: theme.spacing(1),
-    },
-    button: {
-      fill: theme.palette.custom.grey,
-      marginTop: theme.spacing(1) / 2,
-      marginBottom: theme.spacing(1) / 2,
-    },
-  };
+const buttonSxProp = (theme) => ({
+  fill: theme.palette.custom.grey,
+  marginTop: 1 / 2,
+  marginBottom: 1 / 2,
 });
 
 const list = [
@@ -64,7 +35,6 @@ const list = [
 ];
 
 function IDE(props) {
-  const classes = useStyles();
   const [state] = useContext();
   const pages = state.get("pages");
   const [started, setStarted] = useState(pages.started);
@@ -72,17 +42,31 @@ function IDE(props) {
 
   return (
     <>
-      <div className={classes.root}>
+      <Grid sx={{ display: "flex" }}>
         <Menu list={list} title="IDE" />
-        <div className={classes.panel}>
-          <main className={classes.main}>{props.children}</main>
-        </div>
-      </div>
+        <Grid sx={{ flex: 1, display: "flex", flexDirection: "column" }}>
+          <Grid sx={{ flex: 1, padding: 1 }}>{props.children}</Grid>
+        </Grid>
+      </Grid>
       <Drawer
         variant="persistent"
         anchor={"right"}
         open={props.anchor === undefined ? true : props.anchor}
-        classes={{ paper: classes.drawer }}
+        sx={{
+          "& .MuiDrawer-paper": {
+            top: (theme) =>
+              (window.innerHeight * ratio) / 2 - height / 2 + theme.spacing(1),
+            height,
+            borderTopLeftRadius: "5px",
+            borderBottomLeftRadius: "5px",
+            background: "#353e48",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-between",
+            paddingTop: 1,
+            paddingBottom: 1,
+          },
+        }}
       >
         <Box>
           <ListItem
@@ -121,24 +105,24 @@ function IDE(props) {
               setStarted((pages.started = !started));
             }}
           >
-            {!started && <PlayCircleFilledIcon className={classes.button} />}
-            {started && <PauseCircleFilledIcon className={classes.button} />}
+            {!started && <PlayCircleFilledIcon sx={buttonSxProp} />}
+            {started && <PauseCircleFilledIcon sx={buttonSxProp} />}
           </ListItem>
           <ListItem button>
-            <ViewListIcon className={classes.button} />
+            <ViewListIcon sx={buttonSxProp} />
           </ListItem>
           <ListItem button>
-            <GitHubIcon className={classes.button} />
+            <GitHubIcon sx={buttonSxProp} />
           </ListItem>
           <ListItem button>
-            <ImportExportIcon className={classes.button} />
+            <ImportExportIcon sx={buttonSxProp} />
           </ListItem>
           <ListItem button>
             <PostmanIcon />
           </ListItem>
         </Box>
         <ListItem button>
-          <SaveIcon className={classes.button} />
+          <SaveIcon sx={buttonSxProp} />
         </ListItem>
       </Drawer>
       {alert && <AlertMassage key={uuid()} message={alert} />}
