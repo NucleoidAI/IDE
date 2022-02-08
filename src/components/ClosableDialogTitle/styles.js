95136e78e0b87ec31cd4f4b939f820283a63c5d5
Gökhan Demirtaş <gokhan.demirtas@msn.com>
2022-02-08 03:22:52 +0300

    Refactor style ClosableDialogTitle

---
+++ src/components/ClosableDialogTitle/styles.js
@@ -0,0 +1,19 @@
+const styles = {
+  dialogtitle: {
+    margin: 0,
+    marginRight: 1,
+    padding: 3,
+    paddingBottom: 1,
+  },
+  content: {
+    justifyContent: "space-between",
+  },
+  iconbutton: {
+    position: "absolute",
+    right: 1,
+    top: 1,
+    margin: 1,
+  },
+};
+
+export default styles;
