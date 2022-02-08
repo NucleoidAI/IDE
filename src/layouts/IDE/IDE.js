454bf72f6bc7bd810ea2c35f984277b6fc924a9a
Gökhan Demirtaş <gokhan.demirtas@msn.com>
2022-02-08 18:03:33 +0300

    Refactor style IDE layout

--- src/layouts/IDE/IDE.js
+++ src/layouts/IDE/IDE.js
@@ -1,30 +1,22 @@
-import AlertMassage from "../components/AlertMassage";
+import AlertMassage from "../../components/AlertMassage";
 import FolderIcon from "@mui/icons-material/Folder";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import ImportExportIcon from "@mui/icons-material/ImportExport";
-import Menu from "../components/Menu";
+import Menu from "../../components/Menu";
 import PauseCircleFilledIcon from "@mui/icons-material/PauseCircleFilled";
 import PlayCircleFilledIcon from "@mui/icons-material/PlayCircleFilled";
-import PostmanIcon from "../icons/Postman";
+import PostmanIcon from "../../icons/Postman";
 import SaveIcon from "@mui/icons-material/Save";
 import SendIcon from "@mui/icons-material/Send";
 import SettingsEthernetIcon from "@mui/icons-material/SettingsEthernet";
 import StorageIcon from "@mui/icons-material/Storage";
 import ViewCarouselIcon from "@mui/icons-material/ViewCarousel";
 import ViewListIcon from "@mui/icons-material/ViewList";
-import { useContext } from "../context";
+import styles from "./styles";
+import { useContext } from "../../context";
 import { v4 as uuid } from "uuid";
 import { Box, Drawer, Grid, ListItem } from "@mui/material";
 import React, { useState } from "react";
-
-const ratio = 0.6;
-const height = 350;
-
-const buttonSxProp = (theme) => ({
-  fill: theme.palette.custom.grey,
-  marginTop: 1 / 2,
-  marginBottom: 1 / 2,
-});
 
 const list = [
   { title: "API", link: "/ide/api", icon: <SendIcon /> },
@@ -42,31 +34,17 @@ function IDE(props) {
 
   return (
     <>
-      <Grid sx={{ display: "flex" }}>
+      <Grid sx={styles.root}>
         <Menu list={list} title="IDE" />
-        <Grid sx={{ flex: 1, display: "flex", flexDirection: "column" }}>
-          <Grid sx={{ flex: 1, padding: 1 }}>{props.children}</Grid>
+        <Grid sx={styles.content}>
+          <Grid sx={styles.childrens}>{props.children}</Grid>
         </Grid>
       </Grid>
       <Drawer
         variant="persistent"
         anchor={"right"}
         open={props.anchor === undefined ? true : props.anchor}
-        sx={{
-          "& .MuiDrawer-paper": {
-            top: (theme) =>
-              (window.innerHeight * ratio) / 2 - height / 2 + theme.spacing(1),
-            height,
-            borderTopLeftRadius: "5px",
-            borderBottomLeftRadius: "5px",
-            background: "#353e48",
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "space-between",
-            paddingTop: 1,
-            paddingBottom: 1,
-          },
-        }}
+        sx={styles.drawer}
       >
         <Box>
           <ListItem
@@ -105,24 +83,24 @@ function IDE(props) {
               setStarted((pages.started = !started));
             }}
           >
-            {!started && <PlayCircleFilledIcon sx={buttonSxProp} />}
-            {started && <PauseCircleFilledIcon sx={buttonSxProp} />}
+            {!started && <PlayCircleFilledIcon sx={styles.listitem} />}
+            {started && <PauseCircleFilledIcon sx={styles.listitem} />}
           </ListItem>
           <ListItem button>
-            <ViewListIcon sx={buttonSxProp} />
+            <ViewListIcon sx={styles.listitem} />
           </ListItem>
           <ListItem button>
-            <GitHubIcon sx={buttonSxProp} />
+            <GitHubIcon sx={styles.listitem} />
           </ListItem>
           <ListItem button>
-            <ImportExportIcon sx={buttonSxProp} />
+            <ImportExportIcon sx={styles.listitem} />
           </ListItem>
           <ListItem button>
             <PostmanIcon />
           </ListItem>
         </Box>
         <ListItem button>
-          <SaveIcon sx={buttonSxProp} />
+          <SaveIcon sx={styles.listitem} />
         </ListItem>
       </Drawer>
       {alert && <AlertMassage key={uuid()} message={alert} />}
---
+++ src/layouts/IDE/styles.js
@@ -0,0 +1,40 @@
+const ratio = 0.6;
+const height = 350;
+
+const styles = {
+  root: { display: "flex" },
+  content: {
+    flex: 1,
+    display: "flex",
+    flexDirection: "column",
+  },
+  childrens: {
+    flex: 1,
+    padding: 1,
+  },
+  drawer(theme) {
+    return {
+      "& .MuiDrawer-paper": {
+        top: (window.innerHeight * ratio) / 2 - height / 2 + theme.spacing(1),
+        height,
+        borderTopLeftRadius: "5px",
+        borderBottomLeftRadius: "5px",
+        background: "#353e48",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        paddingTop: 1,
+        paddingBottom: 1,
+      },
+    };
+  },
+  listitem(theme) {
+    return {
+      fill: theme.palette.custom.grey,
+      marginTop: 1 / 2,
+      marginBottom: 1 / 2,
+    };
+  },
+};
+
+export default styles;
