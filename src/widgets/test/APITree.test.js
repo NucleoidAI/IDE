5f7085eac1ee2f5341d911b348846949be952085
Gökhan Demirtaş <gokhan.demirtas@msn.com>
2022-03-04 23:48:39 +0300

    Add issue link for uselayouteffect warning in ApiTree test

--- src/widgets/test/APITree.test.js
+++ src/widgets/test/APITree.test.js
@@ -10,6 +10,7 @@ import Enzyme, { shallow } from "enzyme";
 Enzyme.configure({ adapter: new Adapter() });
 jest.mock("../../context");
 
+//https://stackoverflow.com/questions/58070996/how-to-fix-the-warning-uselayouteffect-does-nothing-on-the-server
 jest.mock("react", () => ({
   ...jest.requireActual("react"),
   useLayoutEffect: jest.requireActual("react").useEffect,
