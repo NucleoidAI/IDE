26466b556ab6b9111431c9ba09a005ebd17d68d4
Gökhan Demirtaş <gokhan.demirtas@msn.com>
2022-04-02 18:41:51 +0300

    Update QueryResult

--- src/components/QueryResult.js
+++ src/components/QueryResult.js
@@ -10,7 +10,7 @@ function QueryResult({ json }) {
       displayDataTypes={false}
       displayObjectSize={false}
       quotesOnKeys={false}
-      style={{ border: 0, "overflow-y": "scroll" }}
+      style={{ border: 0, overflowY: "scroll" }}
       iconStyle={"circle"}
     />
   );
