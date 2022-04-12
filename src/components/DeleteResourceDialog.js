30da5b6a7a108924d62f04b46acfce356698e4f1
Gökhan Demirtaş <gokhan.demirtas@msn.com>
2022-04-12 20:41:59 +0300

    Delete unused lines

--- src/components/DeleteResourceDialog.js
+++ src/components/DeleteResourceDialog.js
@@ -2,7 +2,6 @@ import Button from "@mui/material/Button";
 import Dialog from "@mui/material/Dialog";
 import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
-import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
 import { Typography } from "@mui/material";
 import { forwardRef } from "react";
