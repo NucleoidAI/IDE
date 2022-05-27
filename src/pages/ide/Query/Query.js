4b2610002ee8b6942e1ca421dc336952fbaf815c
Gokhan <gokhan.demirtas@msn.com>
2022-05-27 18:08:50 +0300

    Remove unused lines Query page

--- src/pages/ide/Query/Query.js
+++ src/pages/ide/Query/Query.js
@@ -20,35 +20,6 @@ import {
 
 import React, { useEffect, useRef, useState } from "react";
 
-/*
-    editor: {
-      height: height * ratio - theme.spacing(1) / 2,
-    },
-    results: {
-      height: height * (1 - ratio) - theme.spacing(1) / 2,
-      justifyContent: "flex-start",
-      paddingLeft: theme.spacing(1) * 2,
-    },
-    process: {
-      position: "absolute",
-      top: theme.spacing(1) * 2,
-      right: theme.spacing(1) * 2,
-    },
-    run: {
-      position: "relative",
-      bottom: 40 + theme.spacing(1),
-      right: theme.spacing(1),
-    },
-    popupIndicator: {
-      "& span": {
-        "& svg": {
-          "& path": { fill: "#212121" },
-        },
-      },
-    },
-
-*/
-
 function Query() {
   const [result, setResult] = useState();
   const editor = useRef();
