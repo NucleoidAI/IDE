e1913fe31ced883ea1bed4689fc67bb52d847e0d
Gokhan <gokhan.demirtas@msn.com>
2022-06-02 18:51:13 +0300

    Refactor logo

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,16 +1,35 @@
 import React from "react";
+import { Box, Typography } from "@mui/material";
 
 function Logo(props) {
   return (
-    <div>
-      <font face="Segoe UI" color="#28a745" style={{ fontSize: "22px" }}>
-        Nucleoid
-      </font>
-      <font face="Segoe UI" color="#bfbfbf" style={{ fontSize: "18px" }}>
+    <Box
+      sx={{
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        justifyContent: "flex-start",
+      }}
+    >
+      <Box>
+        <Typography display={"inline"} fontSize={"22px"} color={"#28a745"}>
+          Nucleoid
+        </Typography>
         &nbsp;
-        {props.title}
-      </font>
-    </div>
+        <Typography display={"inline"} fontSize={"18px"} color={"#dfdfdf"}>
+          {props.title}
+        </Typography>
+        &nbsp;
+        <Typography
+          display={"inline"}
+          fontSize={"15px"}
+          color={"#dfdfdf"}
+          fontWeight={"lighter"}
+        >
+          <sub>Beta</sub>
+        </Typography>
+      </Box>
+    </Box>
   );
 }
 
