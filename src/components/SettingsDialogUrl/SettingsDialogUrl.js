99e53731bd8ee498e15105a1ed95635d8ea51b9a
Gokhan <gokhan.demirtas@msn.com>
2022-06-17 17:43:29 +0300

    Refactor SettingsDialogUrl page

--- src/components/SettingsDialogUrl/SettingsDialogUrl.js
+++ src/components/SettingsDialogUrl/SettingsDialogUrl.js
@@ -1,10 +1,22 @@
 import React from "react";
+import Settings from "../../settings";
 import styles from "./styles";
-import { Box, Switch, TextField, Typography } from "@mui/material";
+import {
+  Box,
+  FormControlLabel,
+  Switch,
+  TextField,
+  Typography,
+} from "@mui/material";
 
 const SettingsDialogUrl = React.forwardRef((props, urlRef) => {
+  const [npx, setNpx] = React.useState(Settings.runtime());
   const url = urlRef.current;
 
+  React.useEffect(() => {
+    Settings.runtime(npx ? "npx" : "sandbox");
+  }, [npx]);
+  console.log(Settings.runtime());
   return (
     <>
       <TextField
@@ -26,7 +38,16 @@ const SettingsDialogUrl = React.forwardRef((props, urlRef) => {
           justifyContent: "flex-start",
         }}
       >
-        <Typography>select sandbox</Typography> <Switch />
+        <Typography sx={{ pr: 2 }}>Selected runtime</Typography>
+        <FormControlLabel
+          control={
+            <Switch
+              defaultChecked={npx}
+              onChange={(e) => setNpx(e.target.checked)}
+            />
+          }
+          label={npx ? "npx" : "CodeSandbox"}
+        />
       </Box>
     </>
   );
