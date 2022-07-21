6e4b2088fb7eec053610de7a7af1e570e8472b3c
Gokhan <gokhan.demirtas@msn.com>
2022-07-21 21:39:06 +0300

    Split styles MessageDialog

--- src/components/MessageDialog/MessageDialog.js
+++ src/components/MessageDialog/MessageDialog.js
@@ -2,7 +2,7 @@ import Close from "@mui/icons-material/Close";
 import React from "react";
 import Snackbar from "@mui/material/Snackbar";
 import onboardDispatcher from "../Onboard/onboardDispatcher";
-import theme from "../../theme";
+import styles from "./styles";
 import {
   Box,
   IconButton,
@@ -33,52 +33,19 @@ const MessageDialog = ({ message, lifeTime }) => {
       anchorOrigin={{ vertical, horizontal }}
       key={vertical + horizontal}
     >
-      <Paper
-        sx={{
-          backgroundColor: theme.palette.custom.messageBG,
-          color: "rgba(0, 0, 0, 0.87)",
-          fontSize: theme.typography.pxToRem(12),
-          border: "1px solid #dadde9",
-        }}
-        elevation={3}
-      >
-        <Box
-          sx={{
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center",
-          }}
-        >
+      <Paper sx={styles.root} elevation={3}>
+        <Box sx={styles.titleContainer}>
           <Box />
-          <Typography sx={{ pl: 2.5, fontSize: "1rem", fontWeight: "bold" }}>
-            Congrats!
-          </Typography>
+          <Typography sx={styles.titleText}>Congrats!</Typography>
           <IconButton onClick={handleClose}>
             <Close fontSize="small" />
           </IconButton>
         </Box>
-        <Box sx={{ ml: 2.5, mr: 2.5 }}>
-          <Box
-            sx={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <Typography>
-              You've created your APIs with the help of AI
-            </Typography>
-            <span style={{ fontSize: 20, marginLeft: 15 }}>🥳 🎉</span>
-          </Box>
+        <Box sx={styles.content}>
+          <Typography>You've created your APIs with the help of AI</Typography>
+          <span style={{ fontSize: 20, marginLeft: 15 }}>🥳 🎉</span>
         </Box>
-        <Box
-          sx={{
-            maxHeight: "25px",
-            minHeight: "25px",
-            display: "flex",
-            flexDirection: "column-reverse",
-          }}
-        >
+        <Box sx={styles.footer}>
           <ProgressWithTime />
         </Box>
       </Paper>
