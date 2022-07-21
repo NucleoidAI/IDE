b452e3768944db349a00913bee8a391892dfe420
Gokhan <gokhan.demirtas@msn.com>
2022-07-21 22:18:00 +0300

    Split styles GlobalMessageBox

--- src/components/GlobalMessageBox/GlobalMessageBox.js
+++ src/components/GlobalMessageBox/GlobalMessageBox.js
@@ -1,6 +1,6 @@
 import Close from "@mui/icons-material/Close";
 import React from "react";
-import theme from "../../theme";
+import styles from "./styles";
 import {
   Box,
   IconButton,
@@ -11,40 +11,15 @@ import {
 
 const GlobalMessageBox = ({ children, title, handleClose }) => {
   return (
-    <Paper
-      sx={{
-        backgroundColor: theme.palette.custom.messageBG,
-        color: "rgba(0, 0, 0, 0.87)",
-        fontSize: theme.typography.pxToRem(12),
-        border: "1px solid #dadde9",
-        minWidth: "250px",
-      }}
-      elevation={3}
-    >
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-          maxHeight: "25px",
-        }}
-      >
-        <Typography sx={{ pl: 1.5, fontSize: "1rem", fontWeight: "bold" }}>
-          {title}
-        </Typography>
+    <Paper sx={styles.root} elevation={3}>
+      <Box sx={styles.title}>
+        <Typography sx={styles.titleText}>{title}</Typography>
         <IconButton onClick={handleClose}>
           <Close fontSize="small" />
         </IconButton>
       </Box>
-      <Box sx={{ ml: 1.5, mr: 1.5 }}>{children}</Box>
-      <Box
-        sx={{
-          maxHeight: "25px",
-          minHeight: "25px",
-          display: "flex",
-          flexDirection: "column-reverse",
-        }}
-      >
+      <Box sx={styles.content}>{children}</Box>
+      <Box sx={styles.footer}>
         <ProgressWithTime />
       </Box>
     </Paper>
