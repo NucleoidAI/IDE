b6bf188b7c8e7754f0087710e957289cb4cdf5b6
Gokhan <gokhan.demirtas@msn.com>
2022-07-23 01:24:22 +0300

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-/* eslint-disable */
+/* eslint-disable no-alert */
 self.addEventListener("install", (e) => console.log("pwa installed."));
 self.addEventListener("fetch", (event) => {});
 /* eslint-enable */
