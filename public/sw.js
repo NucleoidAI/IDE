bd48f3b28752c0835387cba08075fedcbcb637b3
Gokhan <gokhan.demirtas@msn.com>
2022-07-23 01:36:35 +0300

    Disable eslint in serviceWorker

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-/* eslint-disable no-alert */
+/* eslint-disable-next-line no-restricted-globals */
 self.addEventListener("install", (e) => console.log("pwa installed."));
+/* eslint-disable-next-line no-restricted-globals */
 self.addEventListener("fetch", (event) => {});
-/* eslint-enable */
