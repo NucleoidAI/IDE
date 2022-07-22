048bdb7c20028069587a781c3cc8bd88e3c94c52
Gokhan <gokhan.demirtas@msn.com>
2022-07-22 16:44:35 +0300

    Add workers and manifest

--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@ import "./index.css";
 import App from "./App";
 import React from "react";
 import ReactDOM from "react-dom";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+serviceWorkerRegistration.register();
