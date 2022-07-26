195a036c0c39ea4edbd25a79c34d7d5b85ed21f9
Gokhan <gokhan.demirtas@msn.com>
2022-07-26 18:50:18 +0300

    Update padding

--- src/components/TabPanel.js
+++ src/components/TabPanel.js
@@ -3,7 +3,13 @@ import Box from "@mui/material/Box";
 const TabPanel = (props) => {
   const { children, value, index } = props;
 
-  return <>{value === index && <Box sx={{ pl: 2 }}>{children}</Box>}</>;
+  return (
+    <>
+      {value === index && (
+        <Box sx={{ pl: 2, height: "100%", bgcolor: "#424242d1" }}>{children}</Box>
+      )}
+    </>
+  );
 };
 
 export default TabPanel;
