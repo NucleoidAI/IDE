9938d2ed843d882f30293eaa222b73ca18b0d46a
Gokhan <gokhan.demirtas@msn.com>
2022-07-27 17:19:01 +0300

    Upgrade React deps

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import "./index.css";
 import App from "./App";
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
