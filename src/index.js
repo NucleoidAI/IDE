6f3765db71fcf9baee8c0d287acf8a9b3df87a84
Gokhan <gokhan.demirtas@msn.com>
2022-08-30 00:29:15 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import "./index.css";
 import App from "./App";
 import React from "react";
 import { createRoot } from "react-dom/client";
-import vfs from "./vfs";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
