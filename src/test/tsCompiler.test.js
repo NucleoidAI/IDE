57860746660fb69ae1b308c380bcf340ee09f25e
322332 <gokhan.demirtas@msn.com>
2022-08-31 20:51:27 +0300

    Remove parser tests

--- src/test/tsCompiler.test.js
+++
@@ -1,9 +0,0 @@
-import tsCompiler, { contextToMap } from "tsCompiler";
-import State from "state";
-
-test("Should return true if compiler init", async () => {
-  const state = State.withSample();
-  const nuc = state.get("nucleoid");
-  const compile = tsCompiler.compile(nuc);
-  console.log(compile);
-});
