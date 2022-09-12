4f03909a8e8e77469ae37be2a07f86b7ac58ddd2
Can Mingir <canmingir@gmail.com>
2022-09-12 08:54:23 -0400

    Run tests before push

--- scripts/pre-push.js
+++ scripts/pre-push.js
@@ -4,6 +4,7 @@ const { execSync } = require("child_process");
 try {
   execSync("npm run lint");
   execSync("npm run format");
+  execSync("npm test");
 } catch (err) {
   console.log(err.stdout.toString());
   process.exit(1);
