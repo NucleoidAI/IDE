72bc21e1d74bb2400c7cd1e1062c01c901b0181a
322332 <gokhan.demirtas@msn.com>
2022-09-21 03:15:07 +0300

    Update OpenSwaggerDialog.js

--- src/components/OpenSwaggerDialog/OpenSwaggerDialog.js
+++ src/components/OpenSwaggerDialog/OpenSwaggerDialog.js
@@ -2,13 +2,26 @@ import OpenSandboxButton from "../../components/OpenSandboxButton";
 import OpenSwaggerButton from "../../components/OpenSwaggerButton";
 import React from "react";
 import Settings from "../../settings";
+import { useEvent, publish } from "../../hooks/useEvent";
 
 function OpenSwaggerDialog(props) {
   const { small } = props;
+
+  const event = useEvent("SWAGGER_DIALOG");
+  console.log(event, "from openswgaerdialog");
+
+  const openSwaggerDialog = () => {
+    publish("SWAGGER_DIALOG", { open: true });
+  };
+
   return (
     <>
-      {Settings.runtime() === "npx" && <OpenSwaggerButton small={small} />}
-      {Settings.runtime() === "sandbox" && <OpenSandboxButton small={small} />}
+      {Settings.runtime() === "npx" && (
+        <OpenSwaggerButton clickEvent={openSwaggerDialog} small={small} />
+      )}
+      {Settings.runtime() === "sandbox" && (
+        <OpenSandboxButton clickEvent={openSwaggerDialog} small={small} />
+      )}
     </>
   );
 }
