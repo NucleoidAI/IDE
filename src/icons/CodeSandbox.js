ef2ea22f68bd62487b97a2395895e0b38717743d
322332 <gokhan.demirtas@msn.com>
2022-09-22 18:01:21 +0300

    Add disabled prop in icons

--- src/icons/CodeSandbox.js
+++ src/icons/CodeSandbox.js
@@ -1,7 +1,12 @@
 import { ReactComponent as CodeSandboxSVG } from "./CodeSandbox.svg";
 
-function CodeSandbox({ fill }) {
-  return <CodeSandboxSVG style={{ width: 25, height: 25 }} fill={fill} />;
+function CodeSandbox({ fill, disabled }) {
+  return (
+    <CodeSandboxSVG
+      style={{ width: 25, height: 25 }}
+      fill={disabled ? "#6a7178" : fill}
+    />
+  );
 }
 
 export default CodeSandbox;
--- src/icons/Swagger.js
+++ src/icons/Swagger.js
@@ -1,7 +1,12 @@
 import { ReactComponent as SwaggerSVG } from "./Swagger.svg";
 
-function Swagger({ fill }) {
-  return <SwaggerSVG style={{ width: 25, height: 25 }} fill={fill} />;
+function Swagger({ fill, disabled }) {
+  return (
+    <SwaggerSVG
+      style={{ width: 25, height: 25 }}
+      fill={disabled ? "#6a7178" : fill}
+    />
+  );
 }
 
 export default Swagger;
