9d032a4642c3d82a7ebb25cdb1fb8d3f169b696a
322332 <gokhan.demirtas@msn.com>
2022-09-22 20:35:38 +0300

    Create connectionScheduler.js

---
+++ src/connectionScheduler.js
@@ -0,0 +1,50 @@
+import { publish } from "./hooks/useEvent";
+import service from "./service";
+
+const scheduler = {
+  timer: null,
+  start() {
+    if (!this.timer) {
+      service
+        .metrics()
+        .then((data) => {
+          publish("RUNTIME_CONNECTION", {
+            status: true,
+            metrics: data,
+          });
+        })
+        .catch((err) => {
+          this.stop();
+          publish("RUNTIME_CONNECTION", {
+            status: false,
+            metrics: { total: 100, free: 50 },
+          });
+        });
+
+      this.timer = setInterval(() => {
+        service
+          .metrics()
+          .then((data) => {
+            console.log(data);
+            publish("RUNTIME_CONNECTION", {
+              status: true,
+              metrics: data,
+            });
+          })
+          .catch((err) => {
+            this.stop();
+            publish("RUNTIME_CONNECTION", {
+              status: false,
+              metrics: { total: 100, free: 50 },
+            });
+          });
+      }, 60 * 1000);
+    }
+  },
+  stop() {
+    clearInterval(this.timer);
+    this.timer = null;
+  },
+};
+
+export default scheduler;
