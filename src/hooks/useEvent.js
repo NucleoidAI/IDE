93eb1c3698be64915ec629d75c859da1a6beb46e
322332 <gokhan.demirtas@msn.com>
2022-09-23 18:04:10 +0300

    Set useEvent initialState from eventMap

--- src/hooks/useEvent.js
+++ src/hooks/useEvent.js
@@ -1,11 +1,22 @@
 import React from "react";
-import { publish, subscribe } from "../Event";
+
+import { eventMap, publish, subscribe } from "../Event";
 
 const useEvent = (subject = "", initialState = {}) => {
-  const [event, setEvent] = React.useState(initialState);
+  function updateMap() {
+    if (eventMap.has(subject)) {
+      return eventMap.get(subject);
+    } else {
+      eventMap.set(subject, initialState);
+      return eventMap.get(subject);
+    }
+  }
+
+  const [event, setEvent] = React.useState(updateMap());
 
   React.useEffect(() => {
     const subs = subscribe(subject, (event) => {
+      eventMap.set(subject, event);
       setEvent({ ...event });
     });
 
