c9b29189d82fad9cda7994196e9d500149ab06e7
322332 <gokhan.demirtas@msn.com>
2022-09-23 19:47:28 +0300

    Update connectionScheduler.js

--- src/connectionScheduler.js
+++ src/connectionScheduler.js
@@ -4,24 +4,24 @@ import service from "./service";
 const scheduler = {
   timer: null,
   start() {
-    if (!this.timer) {
-      service
-        .metrics()
-        .then((data) => {
-          publish("RUNTIME_CONNECTION", {
-            status: true,
-            metrics: data,
-          });
-        })
-        .catch((err) => {
-          this.stop();
-          publish("RUNTIME_CONNECTION", {
-            status: false,
-            metrics: { total: 100, free: 50 },
-          });
-          publish("SWAGGER_DIALOG", { state: false });
+    service
+      .metrics()
+      .then((data) => {
+        publish("RUNTIME_CONNECTION", {
+          status: true,
+          metrics: data,
         });
+      })
+      .catch((err) => {
+        this.stop();
+        publish("RUNTIME_CONNECTION", {
+          status: false,
+          metrics: { total: 100, free: 50 },
+        });
+        publish("SWAGGER_DIALOG", { state: false });
+      });
 
+    if (!this.timer) {
       this.timer = setInterval(() => {
         service
           .metrics()
