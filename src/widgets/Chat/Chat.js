eb9f9ad886240c293fe172d3c41f8bc46e4db2f2
322332 <gokhan.demirtas@msn.com>
2023-01-23 23:11:22 +0300

    Add openai in chat

--- src/widgets/Chat/Chat.js
+++ src/widgets/Chat/Chat.js
@@ -1,17 +1,28 @@
 import React from "react";
+import { deepCopy } from "../../utils/DeepCopy";
+import service from "../../service";
+import { useContext } from "../../context/context";
 import { useEvent } from "@nucleoidjs/synapses";
+
 import ChatWindow, {
   handleAddResponseMessage,
 } from "../../components/ChatWindow";
 
 const Chat = () => {
   const [open, setOpen] = useEvent("chatWindow", false);
+  const [context] = useContext();
 
-  React.useEffect(() => {
-    setTimeout(() => {
-      handleAddResponseMessage("hello");
-    }, 700);
-  }, []);
+  const generateContent = () => {
+    const nucFunctions = deepCopy(context.nucleoid.functions);
+
+    return nucFunctions.map((item) => item.definition).join("\n");
+  };
+
+  const handleNewUserMessage = (message) => {
+    service
+      .openai(generateContent(), message)
+      .then((res) => handleAddResponseMessage(res.data.text));
+  };
 
   const handleClose = () => {
     setOpen("chatWindow", false);
@@ -23,7 +34,7 @@ const Chat = () => {
         open={open}
         handleClose={handleClose}
         history={[{ message: "Welcome to nucleoid chat", user: false }]}
-        handleNewUserMessage={(message) => console.log(message)}
+        handleNewUserMessage={handleNewUserMessage}
       />
     </>
   );
