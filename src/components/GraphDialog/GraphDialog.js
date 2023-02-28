51d51b54f989dc4fe7205b179af0db4d9f767d97
Can Mingir <canmingir@gmail.com>
2023-02-28 17:43:38 -0500

    Move to graph folder

--- src/components/GraphDialog/GraphDialog.js
+++ src/components/GraphDialog/GraphDialog.js
@@ -27,7 +27,7 @@ const GraphDialog = () => {
             width={"100%"}
             height={"100%"}
             style={{ border: "none" }}
-            src={"https://nucleoid.com/graph"}
+            src={"/ide/graph/index.html"}
           ></iframe>
         </DialogContent>
       </Dialog>
