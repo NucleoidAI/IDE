9ef39c8914e7d0e202cdec0568d4bee9bec5ed79
Can Mingir <canmingir@gmail.com>
2023-03-01 15:43:45 -0500

    Remove graph from beta

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -80,7 +80,7 @@ function Menu(props) {
                 <SmallMenuLinkWithFilter />
               </List>
               <ProjectSelectSmall />
-              {settings.beta() && <GraphButton />}
+              <GraphButton />
               <Box sx={{ height: "100%" }} />
               <OpenSwaggerDialog small />
               <Settings size={"large"} />
@@ -125,7 +125,7 @@ function Menu(props) {
               <MenuLinkWithFilter />
             </List>
             <ProjectSelect />
-            {settings.beta() && <GraphButton />}
+            <GraphButton />
             <Status />
             <OpenSwaggerDialog />
             <Settings size={"large"} />
@@ -151,7 +151,7 @@ function Menu(props) {
                 <SmallMenuLinkWithFilter />
               </List>
               <ProjectSelectSmall />
-              {settings.beta() && <GraphButton />}
+              <GraphButton />
               <Box sx={{ height: "100%" }} />
               <OpenSwaggerDialog small />
               <Settings size={"large"} />
@@ -183,7 +183,7 @@ function Menu(props) {
               <MenuLinkWithFilter />
             </List>
             <ProjectSelect />
-            {settings.beta() && <GraphButton />}
+            <GraphButton />
             <Status />
             <OpenSwaggerDialog />
             <Settings size={"large"} />
