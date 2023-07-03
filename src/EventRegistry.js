ee4267a18e10277579c1ef1497d73da9dc27badb
durulkoca <durulkoca@gmail.com>
2023-07-03 14:20:46 +0300

    Update EventRegistry.js
    remove comment line

--- src/EventRegistry.js
+++ src/EventRegistry.js
@@ -8,7 +8,6 @@ const EventRegistry = () => {
 
   React.useEffect(() => {
     subscribe("CONTEXT_CHANGED", ({ files }) => {
-      //  console.log("CONTEXT_CHANGED", vfs);
       files.forEach(({ key, value }) => {
         if (value) {
           vfs.upsert(key, value);
