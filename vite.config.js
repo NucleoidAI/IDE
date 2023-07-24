824d67b27afe9ca55997d2113ed36661885a6d45
Can Mingir <canmingir@gmail.com>
2023-07-24 07:42:58 -0400

    Change deployment to S3 bucket

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
-  base: "/ide/",
+  base: "/ide",
   plugins: [react(), svgr()],
 });
