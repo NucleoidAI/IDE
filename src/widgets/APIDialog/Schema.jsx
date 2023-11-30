2b416deb7c6a1af1bc5842d853fa90feaf8d3575
durulkoca <durulkoca@gmail.com>
2023-11-30 21:52:41 +0300

    Add add property functionality to Schema

--- src/widgets/APIDialog/Schema.jsx
+++ src/widgets/APIDialog/Schema.jsx
@@ -1,14 +1,36 @@
-import ChevronRight from "@mui/icons-material/ChevronRight";
-import ExpandMore from "@mui/icons-material/ExpandMore";
-import React from "react";
-import { TreeItem } from "@mui/lab";
-import { TreeView } from "@mui/lab";
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import IconButton from "@mui/material/IconButton";
 import { v4 as uuidv4 } from "uuid";
 
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
+import { TreeItem, TreeView } from "@mui/lab";
 
-const Schema = ({ initialData }) => {
-  const [schemaData, setSchemaData] = useState([]);
+const getTypeStyle = (type) => {
+  return {
+    fontWeight: "normal",
+    color: getTypeColor(type),
+    marginLeft: "auto",
+  };
+};
+
+const getTypeColor = (type) => {
+  switch (type) {
+    case "object":
+      return "red";
+    case "string":
+      return "green";
+    case "integer":
+      return "blue";
+
+    default:
+      return "black";
+  }
+};
+
+const Schema = ({ initialData = {} }) => {
+  const [schemaData, setSchemaData] = useState({});
 
   useEffect(() => {
     const addIdsToSchema = (schema) => {
@@ -19,30 +41,110 @@ const Schema = ({ initialData }) => {
       };
     };
 
-    if (initialData) {
-      const dataWithIds = addIdsToSchema(initialData);
-      setSchemaData(dataWithIds);
+    if (!schemaData || Object.keys(schemaData).length === 0) {
+      if (Object.keys(initialData).length === 0) {
+        setSchemaData({
+          type: "object",
+          properties: [],
+        });
+      } else {
+        const dataWithIds = addIdsToSchema(initialData);
+        setSchemaData(dataWithIds);
+      }
     }
-  }, [initialData]);
+  }, [initialData, schemaData]);
+  const addProperty = (newProperty) => {
+    setSchemaData((currentData) => {
+      const updatedProperties = currentData.properties
+        ? [...currentData.properties]
+        : [];
 
-  const renderTree = (node) => (
+      updatedProperties.push({ ...newProperty, id: uuidv4() });
+
+      return {
+        ...currentData,
+        properties: updatedProperties,
+      };
+    });
+    console.log("schemaData", schemaData);
+  };
+
+  const renderTree = (node, level = 0) => (
     <TreeItem
       key={node.id}
-      nodeId={node.id}
-      label={`${node.name} (${node.type})`}
+      nodeId={level === 0 ? "1" : node.id}
+      label={
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
+        >
+          <span>{node.name}</span>
+          <div style={{ display: "flex", alignItems: "center" }}>
+            {(node.type === "object" || node.type === "array") && (
+              <IconButton
+                size="small"
+                onClick={(e) => {
+                  e.preventDefault();
+                  e.stopPropagation();
+                  addProperty({ type: "string", name: "newProperty" }, node.id);
+                }}
+              >
+                <AddCircleOutlineIcon fontSize="small" />
+              </IconButton>
+            )}
+            <span style={getTypeStyle(node.type)}>({node.type})</span>
+          </div>
+        </div>
+      }
     >
       {Array.isArray(node.properties)
-        ? node.properties.map((childNode) => renderTree(childNode))
+        ? node.properties.map((childNode) => renderTree(childNode, level + 1))
         : null}
     </TreeItem>
   );
+  const schemaOutput = () => {
+    const removeIds = (node) => {
+      const { id, properties, ...rest } = node;
+      if (node.type === "object" && properties) {
+        return { ...rest, properties: properties.map(removeIds) };
+      }
+      return rest;
+    };
+    return removeIds(schemaData);
+  };
 
+  const schemaOutputWithIDs = () => schemaData;
+
+  Schema.addProperty = addProperty;
+  Schema.schemaOutput = schemaOutput;
+  Schema.schemaOutputWithIDs = schemaOutputWithIDs;
   return (
     <TreeView
-      defaultCollapseIcon={<ExpandMore />}
-      defaultExpandIcon={<ChevronRight />}
+      defaultCollapseIcon={<ExpandMoreIcon />}
+      defaultExpandIcon={<ChevronRightIcon />}
+      defaultExpanded={["1"]}
+      onNodeToggle={() => console.log(schemaOutputWithIDs())}
+      sx={{
+        flexGrow: 1,
+        overflowY: "auto",
+        width: "100%",
+        ".MuiTreeItem-root": {
+          alignItems: "center",
+        },
+        ".MuiTreeItem-content": {
+          width: "100%",
+          display: "flex",
+          justifyContent: "space-between",
+        },
+        ".MuiTreeItem-label": {
+          width: "100%",
+        },
+      }}
     >
-      {schemaData?.properties?.map((node) => renderTree(node))}
+      {renderTree(schemaData, 0)}
     </TreeView>
   );
 };
