d5bfebcfe559e639ab50c12ec4c5bf1f929f4763
322332 <gokhan.demirtas@msn.com>
2023-12-10 00:48:15 +0300

    Refactor sample

--- src/sample.js
+++ src/sample.js
@@ -26,7 +26,7 @@ export const api = [
       },
     ],
     response: {
-      type: "TS",
+      type: "OPENAPI",
       schema: {
         name: "User",
         type: "object",
@@ -47,202 +47,28 @@ export const api = [
       }`,
   },
   {
-    path: "/items",
-    method: "GET",
-    params: [
-      {
-        in: "query",
-        type: "string",
-        required: true,
-        name: "name",
+    path: "/orders",
+    method: "POST",
+    request: {
+      type: "OPENAPI",
+      schema: {
+        type: "object",
+        properties: [
+          { name: "qty", type: "number" },
+          { name: "test", type: "string" },
+        ],
       },
-    ],
+    },
     response: {
       type: "OPENAPI",
       schema: {
-        name: "Item",
         type: "object",
         properties: [
-          { name: "name", type: "string" },
-          { name: "barcode", type: "string" },
+          { name: "qty", type: "number" },
+          { name: "test", type: "string" },
         ],
       },
     },
-    "x-nuc-action": `function action(req: { query: { name: string } }): any {
-      const name = req.query.name;
-      return Item.filter(item => item.name === name);
-    }`,
-  },
-  {
-    path: "/items",
-    method: "POST",
-    request: {
-      type: "TS",
-      schema: {
-        name: "Item",
-        type: "object",
-        properties: [
-          { name: "name", type: "string" },
-          { name: "barcode", type: "string" },
-        ],
-      },
-    },
-    response: {
-      type: "TS",
-      schema: {
-        name: "Item",
-        type: "object",
-        properties: [
-          { name: "name", type: "string" },
-          { name: "barcode", type: "string" },
-        ],
-      },
-    },
-    "x-nuc-action": `function action(req: { body: { name: string, barcode: string } }): any {
-      const name = req.body.name;
-      const barcode = req.body.barcode;
-      const check = Item.find(i => i.barcode === barcode);
-      if(check) {
-        throw "DUPLICATE_BARCODE";
-      }
-      return new Item(name, barcode);
-    }`,
-  },
-  {
-    path: "/items/{item}",
-    method: "GET",
-    params: [
-      {
-        name: "item",
-        in: "path",
-        type: "string",
-        required: true,
-        description: "item",
-      },
-    ],
-    response: {
-      type: "TS",
-      schema: {
-        name: "Item",
-        type: "object",
-        properties: [
-          { name: "name", type: "string" },
-          { name: "barcode", type: "string" },
-        ],
-      },
-    },
-    "x-nuc-action": `function action(req: { params: { item: string } }): any {
-      const item = req.params.item;
-      return Item[item];
-    }`,
-  },
-  {
-    path: "/items/{item}",
-    method: "PUT",
-    request: {
-      type: "TS",
-      schema: {
-        name: "Item",
-        type: "object",
-        properties: [
-          { name: "name", type: "string" },
-          { name: "barcode", type: "string" },
-        ],
-      },
-    },
-    response: {
-      type: "TS",
-      schema: {
-        name: "Item",
-        type: "object",
-        properties: [
-          { name: "name", type: "string" },
-          { name: "barcode", type: "string" },
-        ],
-      },
-    },
-    "x-nuc-action": `function action(req: { params: { item: string }, body: { name: string, barcode: string } }): any {
-      const item = Item[req.params.item];
-      const name = req.body.name;
-      const barcode = req.body.barcode;
-      if (!item) {
-        return;
-      }
-      const check = Item.find(
-        i => i.barcode === barcode && i.barcode !== item.barcode
-      );
-      if (check) {
-        throw "DUPLICATE_BARCODE";
-      }
-      item.name = name;
-      item.barcode = barcode;
-      return item;
-    }`,
-  },
-  {
-    path: "/items/{item}",
-    method: "DEL",
-    request: {
-      type: "TS",
-      schema: {
-        name: "Item",
-        type: "object",
-        properties: [
-          { name: "name", type: "string" },
-          { name: "barcode", type: "string" },
-        ],
-      },
-    },
-    response: {
-      type: "TS",
-      schema: {
-        name: "Item",
-        type: "object",
-        properties: [
-          { name: "name", type: "string" },
-          { name: "barcode", type: "string" },
-        ],
-      },
-    },
-    "x-nuc-action": `function action(req: { params: { item: string } }): void {
-      const item = req.params.item;
-      delete Item[item];
-    }`,
-  },
-  {
-    path: "/orders",
-    method: "GET",
-    params: [],
-    response: {
-      type: "TS",
-      schema: {
-        name: "Order",
-        type: "array",
-        properties: [{ name: "item", type: "ref", ref: "Item" }],
-      },
-    },
-    "x-nuc-action": `function action(req: any): any {
-      return Order;
-    }`,
-  },
-  {
-    path: "/orders",
-    method: "POST",
-    request: {
-      type: "object",
-      properties: {
-        type: "TS",
-        schema: {
-          name: "Order",
-          type: "object",
-          properties: [
-            { name: "qty", type: "number" },
-            { name: "item", type: "ref", ref: "Item" },
-          ],
-        },
-      },
-    },
-    response: "Order",
     "x-nuc-action": `function action(req: { body: { item: string, qty: number } }): any {
       const item = Item[req.body.item];
       const qty = req.body.qty;
@@ -250,101 +76,6 @@ export const api = [
         throw "INVALID_ITEM";
       }
       return new Order(item, qty);
-    }`,
-  },
-  {
-    path: "/orders/{order}",
-    method: "GET",
-    params: [
-      {
-        name: "order",
-        in: "path",
-        type: "string",
-        required: true,
-        description: "order",
-      },
-    ],
-    response: {
-      type: "TS",
-      schema: {
-        name: "Order",
-        type: "object",
-        properties: [
-          { name: "qty", type: "number" },
-          { name: "item", type: "ref", ref: "Item" },
-        ],
-      },
-    },
-    "x-nuc-action": `function action(req: { params: { order: string } }): any {
-      const order = req.params.order;
-      return Order[order];
-    }`,
-  },
-  {
-    path: "/orders/{order}",
-    method: "PUT",
-    params: [
-      {
-        name: "order",
-        in: "path",
-        type: "string",
-        required: true,
-        description: "Order id",
-      },
-    ],
-    request: {
-      type: "TS",
-      schema: {
-        name: "Order",
-        type: "object",
-        properties: [
-          { name: "qty", type: "number" },
-          { name: "item", type: "ref", ref: "Item" },
-        ],
-      },
-    },
-    response: "Order",
-    "x-nuc-action": `function action(req: { params: { order: string }, body: { item: string, qty: number } }): any {
-      const order = Order[req.params.order];
-      const item = Item[req.body.item];
-      const qty = req.body.qty;
-      if (!order) {
-        return;
-      }
-      if (!item) {
-        throw "INVALID_ITEM";
-      }
-      order.item = item;
-      order.qty = qty;
-      return order;
-    }`,
-  },
-  {
-    path: "/orders/{order}",
-    method: "DEL",
-    params: [
-      {
-        name: "order",
-        in: "path",
-        type: "string",
-        required: true,
-        description: "Order id",
-      },
-    ],
-    response: {
-      type: "TS",
-      schema: {
-        name: "Order",
-        type: "object",
-        properties: [
-          { name: "qty", type: "number" },
-          { name: "item", type: "ref", ref: "Item" },
-        ],
-      },
-    },
-    "x-nuc-action": `function action(req: { params: { order: string } }): void {
-      const order = req.params.order;
-      delete Order[order];
     }`,
   },
 ];
@@ -386,11 +117,6 @@ export const types = [
         {
           name: "qty",
           type: "number",
-        },
-        {
-          name: "item",
-          type: "ref",
-          ref: "Item",
         },
       ],
     },
@@ -450,15 +176,6 @@ export const types = [
           type: "number",
         },
       ],
-    },
-  },
-  {
-    name: "Users",
-    type: "OPENAPI",
-    schema: {
-      name: "Users",
-      type: "array",
-      properties: [{ name: "user", type: "ref", ref: "User" }],
     },
   },
 ];
