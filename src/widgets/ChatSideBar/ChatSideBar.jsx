6a85df44438e8237d2643004b4a8fed5c2194642
durulkoca <durulkoca@gmail.com>
2024-02-08 21:33:37 +0300

    Add Chat Sidebar Implementation

--- src/widgets/ChatSideBar/ChatSideBar.jsx
+++ src/widgets/ChatSideBar/ChatSideBar.jsx
@@ -1,10 +1,63 @@
 import React from "react";
-import { Box } from "@mui/material";
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+} from "@mui/material";
+import Settings from "../../components/Settings";
+import Logo from "../../components/Logo";
+
+const ChatHistory = ({ chats }) => {
+  const handleChatClick = (chatId) => {
+    console.debug(`Chat clicked: ${chatId}`);
+  };
+
+  return (
+    <List sx={{ width: "100%" }}>
+      {chats.map((chat) => (
+        <ListItemButton
+          key={chat.chatId}
+          onClick={() => handleChatClick(chat.chatId)}
+          sx={{
+            paddingX: 0,
+
+            "& .MuiListItemText-root": {
+              overflow: "hidden",
+              whiteSpace: "nowrap",
+              textOverflow: "ellipsis",
+            },
+          }}
+        >
+          <ListItemText primary={chat.chatTitle} />
+        </ListItemButton>
+      ))}
+    </List>
+  );
+};
 
 const ChatSideBar = () => {
+  const chatData = [
+    { chatId: "1", chatTitle: "What is the circumfrance of the Earth?" },
+    { chatId: "2", chatTitle: "How to center a div?" },
+    { chatId: "3", chatTitle: "What do blind people see in their dreams?" },
+  ];
+
   return (
-    <Box sx={{ height: "100%" }} p={2} bgcolor="lightgrey">
-      Sidebar Content
+    <Box
+      sx={{ display: "flex", flexDirection: "column", height: "100%" }}
+      p={2}
+    >
+      <Box sx={{ flexGrow: 0 }}>
+        <Logo title={"Chat"} />
+      </Box>
+      <Box sx={{ flexGrow: 1, overflowY: "auto" }}>
+        <ChatHistory chats={chatData} />
+      </Box>
+      <Box sx={{ flexGrow: 0 }}>
+        <Settings size={"large"} />
+      </Box>
     </Box>
   );
 };
