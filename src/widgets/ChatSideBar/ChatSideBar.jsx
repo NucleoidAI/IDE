315a8158149fde418600a3c34b684fb72f01f3c3
durulkoca <durulkoca@gmail.com>
2024-02-12 20:11:29 +0300

    Add new chat button to sidebar

--- src/widgets/ChatSideBar/ChatSideBar.jsx
+++ src/widgets/ChatSideBar/ChatSideBar.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import {
   Box,
   List,
+  Button,
   ListItemButton,
   ListItemText,
+  Divider,
+  Icon,
   useTheme,
 } from "@mui/material";
 import Settings from "../../components/Settings";
 import Logo from "../../components/Logo";
 import { publish } from "@nucleoidjs/react-event";
+import AddIcon from "@mui/icons-material/Add";
 
 const ChatHistory = ({ chats }) => {
   const theme = useTheme();
@@ -52,6 +56,10 @@ const ChatSideBar = () => {
     { chatId: "3", chatTitle: "Problem of criterion" },
   ];
 
+  const handleCreateNewChat = () => {
+    publish("CHAT_ID_CHANGED", "-1");
+  };
+
   return (
     <Box
       sx={{
@@ -65,6 +73,18 @@ const ChatSideBar = () => {
     >
       <Box sx={{ flexGrow: 0 }}>
         <Logo title={"Chat"} />
+      </Box>
+
+      <Box sx={{ flexGrow: 0, my: 2 }}>
+        <Divider sx={{ my: 2 }} />
+        <Button
+          variant="outlined"
+          startIcon={<AddIcon />}
+          onClick={handleCreateNewChat}
+          fullWidth
+        >
+          Create New Chat
+        </Button>
       </Box>
       <Box sx={{ flexGrow: 1, overflowY: "auto" }}>
         <ChatHistory chats={chatData} />
