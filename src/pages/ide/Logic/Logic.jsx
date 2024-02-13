d8261ac14cd1d523f19ee5200e2294f8a3b57f29
Halil Ibrahim Cengel <halilibrahimcengel@icloud.com>
2024-02-13 17:56:18 +0300

    Add Logic Widget and Dialog to VerticalSplitLayout

--- src/pages/ide/Logic/Logic.jsx
+++ src/pages/ide/Logic/Logic.jsx
@@ -1,14 +1,25 @@
-import { Box } from "@mui/system";
-import Editor from "../../../widgets/Editor";
+import LogicDialog from "../../../widgets/LogicDialog/LogicDialog";
 import LogicTree from "../../../widgets/LogicTree";
+import QueryEditor from "../../../widgets/QueryEditor";
 import React from "react";
 import VerticalSplitLayout from "../../../layouts/VerticalSplitLayout";
+import actions from "../../../actions";
+import { useContext } from "../../../context/context";
+
 function Logic() {
+  const [, dispatch] = useContext();
+
+  function openLogicDialog() {
+    dispatch({
+      type: actions.openLogicDialog,
+    });
+  }
+
   return (
     <VerticalSplitLayout
-      dialog={<Box />}
-      content1={<LogicTree />}
-      content2={<Editor name={"api"} api />}
+      dialog={<LogicDialog />}
+      content1={<LogicTree openLogicDialog={openLogicDialog} />}
+      content2={<QueryEditor logic />}
     />
   );
 }
