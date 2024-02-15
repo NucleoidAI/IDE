775675a792604cd770f1831cbc12e186f43bcdd2
Can Mingir <canmingir@gmail.com>
2024-02-15 06:39:53 -0500

    Build with manual chunks

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,19 @@ import svgr from "vite-plugin-svgr";
 export default defineConfig({
   base: "/ide",
   plugins: [react(), svgr()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+    },
+  },
 });
