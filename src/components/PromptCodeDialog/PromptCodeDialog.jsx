65e924942b30e1bffe00275e63f6852cfca87d6d
Halil Ibrahim Cengel <halilibrahimcengel@icloud.com>
2024-02-16 12:40:57 +0300

    Rename CodeEditor component to PromptCodeDialog

---
+++ src/components/PromptCodeDialog/PromptCodeDialog.jsx
@@ -0,0 +1,150 @@
+import CodeEditor from "../CodeEditor/CodeEditor";
+import Dialog from "@mui/material/Dialog";
+import DialogContent from "@mui/material/DialogContent";
+import DoneOutlineIcon from "@mui/icons-material/DoneOutline";
+import IconButton from "@mui/material/IconButton";
+import PromptInput from "../PromptInput/PromptInput";
+import Slide from "@mui/material/Slide";
+import { alpha } from "@mui/material/styles";
+import { useContext } from "../../context/context";
+
+import {
+  Box,
+  Card,
+  CardContent,
+  CardHeader,
+  Divider,
+  Stack,
+} from "@mui/material";
+import { useEffect, useState } from "react";
+
+function PromptCodeDialog({
+  handleSendAIClick,
+  handleSaveAIResponse,
+  handlePromptChange,
+  onCodeEditorChange,
+  setPromptValue,
+  promptValue,
+  loading,
+  logic,
+  openAI,
+  logo,
+  title,
+  inputPlaceHolder,
+  setEditorRef,
+  isCodeGenerated,
+  handleClose,
+}) {
+  const [open, setOpen] = useState(false);
+  const [context] = useContext();
+
+  function initDialog() {
+    if (logic) {
+      setOpen(Boolean(context.get("pages.logic.dialog.open")));
+    }
+    if (openAI) {
+      setOpen(Boolean(context.get("pages.api.openAIDialog.open")));
+    }
+  }
+
+  useEffect(() => {
+    initDialog();
+  });
+
+  return (
+    <Dialog
+      open={open}
+      fullWidth={true}
+      onClose={handleClose}
+      sx={{
+        borderRadius: 4,
+        "& .MuiDialog-paper": { borderRadius: 3 },
+      }}
+    >
+      <DialogContent
+        fullWidth={true}
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          padding: 2,
+          borderRadius: 3,
+          width: "100%",
+        }}
+      >
+        <Stack sx={{ width: 1 }} direction={"column"}>
+          <Card sx={{ width: 1 }}>
+            <CardHeader
+              avatar={<Box component={logo} />}
+              title={title}
+              titleTypographyProps={{ variant: "h5" }}
+            />
+
+            <Slide direction="up" in={isCodeGenerated}>
+              <div>
+                <Divider color="gray" />
+                <CardContent
+                  sx={{
+                    height: isCodeGenerated ? "15rem" : 1,
+                    width: 1,
+                    m: 1,
+                  }}
+                >
+                  <Stack
+                    direction={"column"}
+                    sx={{
+                      width: 1,
+                      height: 1,
+                      display: "flex",
+                      alignItems: "flex-end",
+                    }}
+                  >
+                    <CodeEditor
+                      onCodeEditorChange={onCodeEditorChange}
+                      setEditorRef={setEditorRef}
+                    />
+
+                    <IconButton
+                      onClick={handleSaveAIResponse}
+                      sx={{
+                        width: 32,
+                        height: 32,
+                        mt: 2,
+                        mr: 3,
+                        display: "flex",
+                        alignContent: "start",
+                        justifyItems: "start",
+                        border: `1px solid ${alpha("#209958", 0.7)}`,
+                        backgroundColor:
+                          promptValue !== "" ? alpha("#209958", 0.7) : "",
+                      }}
+                    >
+                      <DoneOutlineIcon
+                        width="32px"
+                        height="32px"
+                        component={loading && "span"}
+                        sx={{
+                          color: alpha("#209958", 1),
+                        }}
+                      />
+                    </IconButton>
+                  </Stack>
+                </CardContent>
+              </div>
+            </Slide>
+          </Card>
+          <PromptInput
+            handleSendAIClick={handleSendAIClick}
+            handlePromptChange={handlePromptChange}
+            setPromptValue={setPromptValue}
+            promptValue={promptValue}
+            isCodeGenerated={isCodeGenerated}
+            loading={loading}
+            inputPlaceHolder={inputPlaceHolder}
+          />
+        </Stack>
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+export default PromptCodeDialog;
