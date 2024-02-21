6439919fcb93c16b31d3f60d551309354df4bc11
Halil Ibrahim Cengel <halilibrahimcengel@icloud.com>
2024-02-21 22:35:44 +0300

    Add EDITOR_LOADING_COMPLETED" event to Mobile page

--- src/pages/ide/Mobile.jsx
+++ src/pages/ide/Mobile.jsx
@@ -31,6 +31,7 @@ const Mobile = () => {
   });
 
   useEffect(() => {
+    publish("EDITOR_LOADING_COMPLETED", true);
     const timer = setTimeout(() => {
       window.location.href = "https://nucleoid.com/docs/get-started";
     }, 5000);
