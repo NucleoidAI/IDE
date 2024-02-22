69b21ced1bfb1f1957b65e558805bdca605d2a9a
Halil Ibrahim Cengel <halilibrahimcengel@icloud.com>
2024-02-23 00:17:06 +0300

    Change OpenAIIcon color

--- src/icons/OpenAI.jsx
+++ src/icons/OpenAI.jsx
@@ -1,7 +1,7 @@
 import { ReactComponent as OpenAISVG } from "./openai.svg";
 
-function OpenAI({ width = 25, height = 25 }) {
-  return <OpenAISVG style={{ width, height }} />;
+function OpenAI({ width = 25, height = 25, fill = "#c3c5c8" }) {
+  return <OpenAISVG style={{ width, height, fill }} />;
 }
 
 export default OpenAI;
