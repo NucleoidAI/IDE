ba66d332ec2ed2e3d8205317a162207918636667
Halil Ibrahim Cengel <halilibrahimcengel@icloud.com>
2024-03-11 20:21:42 +0300

    Add error handling 500 and EXPERT_ERROR cases

--- src/http/expert.js
+++ src/http/expert.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import axiosRetry from "axios-retry";
 import config from "../../config.js";
+import { publish } from "@nucleoidjs/react-event";
 
 const instance = axios.create({
   baseURL: config.expert,
@@ -8,5 +9,32 @@ const instance = axios.create({
 
 axiosRetry(instance, { retries: 3 });
 axios.defaults.headers.common["Content-Type"] = "application/json";
+
+instance.interceptors.response.use(
+  (response) => {
+    if (response.headers["content-type"] === "application/json") {
+      response.data = JSON.parse(response.data);
+    }
+    return response;
+  },
+  async (error) => {
+    if (error.response.status === 500) {
+      if (error.response.data.error === "EXPERT_ERROR") {
+        publish("EXPERT_ERROR_OCCURRED", {
+          status: true,
+          type: error.response.data.type,
+          content: error.response.data.content,
+        });
+      } else {
+        publish("GLOBAL_MESSAGE", {
+          status: true,
+          message: error.message,
+          severity: "error",
+        });
+      }
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default instance;
