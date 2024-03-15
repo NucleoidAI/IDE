28c28800e6bf9b05faa6ab2946f72e3328e951e0
durulkoca <durulkoca@gmail.com>
2024-03-15 12:54:46 +0900

    Move apiTemplates to its own directory

--- src/utils/ConvertProject.js
+++ src/utils/ConvertProject.js
@@ -1,8 +1,9 @@
 import Handlebars from "handlebars";
-import { v4 as uuidv4 } from "uuid";
+import apiTemplates from "../templates/apiTemplates";
 
-import * as fs from "fs";
+//import * as fs from "fs";
 import * as ts from "typescript";
+//import { v4 as uuidv4 } from "uuid";
 
 Handlebars.registerHelper("camelCase", function (str) {
   return str.charAt(0).toLowerCase() + str.slice(1);
@@ -128,106 +129,6 @@ function extractCodeSnippets(messages) {
 
   return { functions, declarations };
 }
-
-const apiTemplates = {
-  listObjects: `
-    {
-      "path": "/{{pluralName}}",
-      "method": "GET",
-      "params": [],
-      "response": {
-        "type": "OPENAPI",
-        "schema": {
-          "name": "{{className}}",
-          "type": "object",
-          "properties": [
-            {{#each properties}}
-            {
-              "name": "{{name}}",
-              "type": "{{type}}"
-            }{{#unless @last}},{{/unless}}
-            {{/each}}
-          ]
-        }
-      },
-      "summary": "List {{pluralName}}",
-      "description": "List {{pluralName}}",
-      "x-nuc-action": "function action() { return {{className}}; }"
-    }
-  `,
-  createObject: `
-    {
-      "path": "/{{pluralName}}",
-      "method": "POST",
-      "params": [],
-      "request": {
-        "type": "OPENAPI",
-        "schema": {
-          "type": "object",
-          "properties": [
-            {{#each properties}}
-            {
-              "name": "{{name}}",
-              "type": "{{type}}"
-            }{{#unless @last}},{{/unless}}
-            {{/each}}
-          ]
-        }
-      },
-      "response": {
-        "type": "OPENAPI",
-        "schema": {
-          "name": "{{className}}",
-          "type": "object",
-          "properties": [
-            {{#each properties}}
-            {
-              "name": "{{name}}",
-              "type": "{{type}}"
-            }{{#unless @last}},{{/unless}}
-            {{/each}}
-          ]
-        }
-      },
-      "summary": "Create a {{singularName}}",
-      "description": "Create a {{singularName}}",
-      "x-nuc-action": "function action(req) { const {{camelCase singularName}}Data = req.body; return new {{className}}({{#each properties}}{{camelCase ../singularName}}Data.{{name}}{{#unless @last}}, {{/unless}}{{/each}}); }"
-    }
-  `,
-  readObject: `
-  {
-    "path": "/{{pluralName}}/{{encloseBraces singularName}}",
-    "method": "GET",
-    "params": [
-      {
-        "name": "{{camelCase singularName}}Id",
-        "in": "path",
-        "type": "string",
-        "required": true,
-        "description": "{{singularName}} ID"
-      }
-    ],
-    "response": {
-      "type": "OPENAPI",
-      "schema": {
-        "name": "{{className}}",
-        "type": "object",
-        "properties": [
-          {{#each properties}}
-          {
-            "name": "{{name}}",
-            "type": "{{type}}"
-          }{{#unless @last}},{{/unless}}
-          {{/each}}
-        ]
-      }
-    },
-    "summary": "Read a {{singularName}}",
-    "description": "Read a {{singularName}}",
-    "x-nuc-action": "function action(req) { const {{camelCase singularName}}Id = req.params.{{camelCase singularName}}Id; return {{className}}[{{camelCase singularName}}Id]; }"
-  }
-`,
-};
 
 function createAPI(functions) {
   const api = [];
