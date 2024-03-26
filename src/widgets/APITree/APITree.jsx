5d9e0855b6554b40279913978be47d9ddab29162
Halil Ibrahim Cengel <halilibrahimcengel@icloud.com>
2024-03-26 21:23:35 +0300

    Fix blank API object handling in APITree

--- src/widgets/APITree/APITree.jsx
+++ src/widgets/APITree/APITree.jsx
@@ -205,126 +205,130 @@ export const compile = (
   rightClickMethod,
   errors
 ) => {
-  const groupedByPath = apiData.reduce((acc, endpoint) => {
-    const parts = endpoint.path.split("/");
-    let currentLevel = acc;
+  if (Object.keys(apiData).length !== 0) {
+    const groupedByPath = apiData.reduce((acc, endpoint) => {
+      const parts = endpoint.path.split("/");
+      let currentLevel = acc;
 
-    if (endpoint.path === "/") {
-      if (!currentLevel["/"]) {
-        currentLevel["/"] = {
-          methods: [],
-          children: {},
-        };
-      }
-      currentLevel["/"].methods.push(endpoint);
-    } else {
-      currentLevel = currentLevel["/"].children;
-
-      parts.forEach((part, idx, arr) => {
-        if (idx !== 0) {
-          const currentPart = "/" + part;
-
-          if (!currentLevel[currentPart]) {
-            currentLevel[currentPart] = {
-              methods: [],
-              children: {},
-            };
-          }
-
-          if (idx === arr.length - 1) {
-            currentLevel[currentPart].methods.push(endpoint);
-          } else {
-            currentLevel = currentLevel[currentPart].children;
-          }
+      if (endpoint.path === "/") {
+        if (!currentLevel["/"]) {
+          currentLevel["/"] = {
+            methods: [],
+            children: {},
+          };
         }
-      });
-    }
+        currentLevel["/"].methods.push(endpoint);
+      } else {
+        currentLevel = currentLevel["/"].children;
 
-    return acc;
-  }, {});
+        parts.forEach((part, idx, arr) => {
+          if (idx !== 0) {
+            const currentPart = "/" + part;
 
-  const renderTree = (data) => {
-    // eslint-disable-next-line
-    let resourceHash;
-    // eslint-disable-next-line
-    const theme = useTheme();
+            if (!currentLevel[currentPart]) {
+              currentLevel[currentPart] = {
+                methods: [],
+                children: {},
+              };
+            }
 
-    return Object.keys(data).map((path) => {
-      const { methods, children } = data[path];
-
-      const methodItems = methods.map((method) => {
-        const payload = { path: method.path, method: method.method };
-        const hash = (resourceHash = window.btoa(JSON.stringify(payload)));
-        map[hash] = payload;
-
-        const error = errors.find((item) => {
-          const [errPath, errMethod] = item.file.fileName.split(".", 2);
-          if (errPath === method.path && errMethod === method.method) {
-            return item;
-          } else {
-            return null;
+            if (idx === arr.length - 1) {
+              currentLevel[currentPart].methods.push(endpoint);
+            } else {
+              currentLevel = currentLevel[currentPart].children;
+            }
           }
         });
+      }
+
+      return acc;
+    }, {});
+
+    const renderTree = (data) => {
+      // eslint-disable-next-line
+      let resourceHash;
+      // eslint-disable-next-line
+      const theme = useTheme();
+
+      return Object.keys(data).map((path) => {
+        const { methods, children } = data[path];
+
+        const methodItems = methods.map((method) => {
+          const payload = { path: method.path, method: method.method };
+          const hash = (resourceHash = window.btoa(JSON.stringify(payload)));
+          map[hash] = payload;
+
+          const error = errors.find((item) => {
+            const [errPath, errMethod] = item.file.fileName.split(".", 2);
+            if (errPath === method.path && errMethod === method.method) {
+              return item;
+            } else {
+              return null;
+            }
+          });
+
+          return (
+            <TreeItem
+              key={hash}
+              nodeId={hash}
+              onContextMenu={(event) => handleContextMenu(event, hash)}
+              sx={{
+                bgcolor:
+                  hash === rightClickMethod &&
+                  theme.palette.custom.apiTreeRightClick,
+              }}
+              label={
+                <Box
+                  sx={{
+                    display: "flex",
+                    flexDirection: "row",
+                    alignItems: "center",
+                    justifyContent: "space-between",
+                  }}
+                >
+                  <Box sx={theme.custom.apiTreeItem}>
+                    <span
+                      style={{
+                        display: "flex",
+                        flexDirection: "row",
+                        alignItems: "center",
+                        justifyContent: "center",
+                      }}
+                    >
+                      {method.method.toUpperCase()}
+                    </span>
+                  </Box>
+                  {error && (
+                    <Tooltip title={error.messageText} placement={"right"}>
+                      <Error sx={{ color: "#8f8f91" }} />
+                    </Tooltip>
+                  )}
+                </Box>
+              }
+            />
+          );
+        });
+
+        const childItems = children ? renderTree(children) : [];
+        expandList.push(path);
 
         return (
           <TreeItem
-            key={hash}
-            nodeId={hash}
-            onContextMenu={(event) => handleContextMenu(event, hash)}
-            sx={{
-              bgcolor:
-                hash === rightClickMethod &&
-                theme.palette.custom.apiTreeRightClick,
-            }}
-            label={
-              <Box
-                sx={{
-                  display: "flex",
-                  flexDirection: "row",
-                  alignItems: "center",
-                  justifyContent: "space-between",
-                }}
-              >
-                <Box sx={theme.custom.apiTreeItem}>
-                  <span
-                    style={{
-                      display: "flex",
-                      flexDirection: "row",
-                      alignItems: "center",
-                      justifyContent: "center",
-                    }}
-                  >
-                    {method.method.toUpperCase()}
-                  </span>
-                </Box>
-                {error && (
-                  <Tooltip title={error.messageText} placement={"right"}>
-                    <Error sx={{ color: "#8f8f91" }} />
-                  </Tooltip>
-                )}
-              </Box>
-            }
+            key={path}
+            nodeId={path}
+            label={<div className="path">{path}</div>}
+            children={[...methodItems, ...childItems]}
+            collapseIcon={<ArrowIcon down />}
+            expandIcon={<ArrowIcon right />}
           />
         );
       });
+    };
 
-      const childItems = children ? renderTree(children) : [];
-      expandList.push(path);
-
-      return (
-        <TreeItem
-          key={path}
-          nodeId={path}
-          label={<div className="path">{path}</div>}
-          children={[...methodItems, ...childItems]}
-          collapseIcon={<ArrowIcon down />}
-          expandIcon={<ArrowIcon right />}
-        />
-      );
-    });
-  };
-
-  return renderTree(groupedByPath);
+    return renderTree(groupedByPath);
+  } else {
+    return null;
+  }
 };
 
 export default APITree;
