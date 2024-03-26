c6c0f954db0bc2da8c37a6a3cdbb7beee04191b3
Halil Ibrahim Cengel <halilibrahimcengel@icloud.com>
2024-03-26 21:23:44 +0300

    Fix blank API object handling in APISettings

--- src/widgets/APISettings/APISettings.jsx
+++ src/widgets/APISettings/APISettings.jsx
@@ -29,16 +29,25 @@ function APISettings() {
 
   useEffect(() => {
     const selected = state.get("pages.api.selected");
-    const selectApi = state
-      .get("nucleoid.api")
-      .find(
+    const contextApis = state.nucleoid.api;
+
+    let selectApi = null;
+
+    if (Array.isArray(contextApis)) {
+      selectApi = contextApis.find(
         (api) => api.path === selected?.path && api.method === selected.method
       );
+    }
 
-    customTypes = [
-      ...(state?.nucleoid?.types || []),
-      ...getTypes(state.get("nucleoid.functions")),
-    ];
+    const tstypes = getTypes(state.get("nucleoid.functions"));
+    const nuctypes = state.get.nucleoid?.types;
+
+    if (Array.isArray(nuctypes)) {
+      customTypes = [...nuctypes, ...tstypes];
+    } else {
+      customTypes = [...tstypes];
+    }
+
     const api = state.get("nucleoid.api");
 
     if (selected) {
