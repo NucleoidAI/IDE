ce4e38b347b3949884ac6c9a20705f25a5345f25
durulkoca <durulkoca@gmail.com>
2024-03-26 15:31:04 +0900

    Connect graph to sandbox dialog

--- src/components/GraphDialog/GraphDialog.jsx
+++ src/components/GraphDialog/GraphDialog.jsx
@@ -1,13 +1,36 @@
 import { DialogContent } from "@mui/material";
 import NucDialog from "../core/NucDialog/NucDialog";
-import React from "react";
-import { useEvent } from "@nucleoidjs/react-event"; //eslint-disable-line
+import sandboxService from "../../sandboxService";
+import { useEvent } from "@nucleoidjs/react-event";
+
+import React, { useEffect, useState } from "react";
 
 const GraphDialog = () => {
   const [event, publish] = useEvent("GRAPH_DIALOG", { open: false });
+  const [graphUrl, setGraphUrl] = useState("");
+
+  useEffect(() => {
+    const fetchGraphUrl = async () => {
+      try {
+        const graphData = await sandboxService.get("graph");
+        const blob = new Blob([JSON.stringify(graphData)], {
+          type: "application/json",
+        });
+        const url = URL.createObjectURL(blob);
+        setGraphUrl(url);
+      } catch (error) {
+        console.error("Error fetching graph data:", error);
+      }
+    };
+
+    if (event.open) {
+      fetchGraphUrl();
+    }
+  }, [event.open]);
 
   const handleClose = () => {
     publish("GRAPH_DIALOG", { open: false });
+    URL.revokeObjectURL(graphUrl);
   };
 
   if (event.open) {
@@ -28,13 +51,15 @@ const GraphDialog = () => {
             alignItems: "center",
           }}
         >
-          <iframe
-            title="graph"
-            width={"100%"}
-            height={"100%"}
-            style={{ border: "none" }}
-            src={"/ide/graph"}
-          ></iframe>
+          {graphUrl && (
+            <iframe
+              title="graph"
+              width={"100%"}
+              height={"100%"}
+              style={{ border: "none" }}
+              src={graphUrl}
+            ></iframe>
+          )}
         </DialogContent>
       </NucDialog>
     );
