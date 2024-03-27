44e9782020580513bb5ed476f6c7cc4486fd3289
Halil Ibrahim Cengel <halilibrahimcengel@icloud.com>
2024-03-27 21:37:07 +0300

    Update state prop to use arrays instead of objects

--- src/state.js
+++ src/state.js
@@ -5,11 +5,11 @@ import { api, declarations, functions, project, types } from "./sample";
 function init() {
   const state = {
     nucleoid: {
-      api: {},
-      types: {},
+      api: [],
+      types: [],
       functions: [],
       project: {},
-      declarations: {},
+      declarations: [],
     },
     pages: {
       started: false,
