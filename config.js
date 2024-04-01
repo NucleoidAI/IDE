928814744a986432e6b8ad7be17c0449a90a6b29
durulkoca <durulkoca@gmail.com>
2024-04-01 17:56:22 +0900

    Create new config entry for oauth

--- config.js
+++ config.js
@@ -3,6 +3,12 @@ const config = {
   base: "/ide",
   api: "http://localhost:3000/api",
   expert: "https://nuc.land/ide/api/expert",
+  oauth: {
+    accessTokenUrl: "http://localhost:3000/oauth",
+    clientId: "0c2844d3d19dc9293fc5",
+    redirectUri: "http://localhost:5173/ide/login",
+    oauthUrl: "https://github.com/login/oauth/authorize",
+  },
 };
 
 export default config;
