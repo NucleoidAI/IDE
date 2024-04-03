36ad2f51901c11d7d03de1c25e308cb561c674c0
durulkoca <durulkoca@gmail.com>
2024-04-04 03:06:28 +0900

    Change onboarding module to unsubscribe later

--- src/utils/onboarding.js
+++ src/utils/onboarding.js
@@ -2,26 +2,35 @@ import { storage } from "@nucleoidjs/webstorage";
 
 import { publish, subscribe } from "@nucleoidai/react-event";
 
-const checkOnboardingStatus = (chat) => {
-  const landingData = storage.get("ide", "landing");
-  const landingLevel = landingData?.level || 0;
-
-  if (
-    landingLevel === 0 &&
-    chat &&
-    chat.messages.filter((message) => message.code).length >= 3
-  ) {
-    storage.set("ide", "landing", { level: 1 });
-    publish("LANDING_LEVEL_ACHIEVED", 1);
-  }
-};
-
 const onboardingModule = () => {
-  const handleChatChange = (chat) => {
+  let subscription;
+
+  const checkOnboardingStatus = (chat) => {
+    const landingData = storage.get("ide", "landing");
+    const landingLevel = landingData?.level || 0;
+
+    if (
+      landingLevel === 0 &&
+      chat &&
+      chat.messages.filter((message) => message.code).length >= 3
+    ) {
+      storage.set("ide", "landing", { level: 1 });
+      publish("LANDING_LEVEL_ACHIEVED", { level: 1 });
+      subscription.unsubscribe();
+    }
+  };
+
+  const handleNewMessage = (chat) => {
     checkOnboardingStatus(chat);
   };
 
-  subscribe("CHAT_SELECTED", handleChatChange);
+  const init = () => {
+    subscription = subscribe("NEW_MESSAGE_RECEIVED", handleNewMessage);
+  };
+
+  return {
+    init,
+  };
 };
 
 export default onboardingModule;
