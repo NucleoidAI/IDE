2060c2dad6ab8280abf2a0ad492fcc860f7f2a24
Halil Ibrahim Cengel <halilibrahimcengel@icloud.com>
2024-04-08 15:25:17 +0300

    Refactor Path module and add unit tests

--- src/utils/test/Path.test.js
+++ src/utils/test/Path.test.js
@@ -1,48 +1,124 @@
-jest.mock("@nucleoidjs/webstorage", () => jest.fn());
 import Path from "../Path.js";
+import { storage } from "@nucleoidjs/webstorage";
 
-test("splits prefix and suffix", () => {
-  const { prefix, suffix } = Path.split("/questions/reviews/{rev3}/items");
-  expect(prefix).toEqual("/questions/reviews/{rev3}");
-  expect(suffix).toEqual("items");
+jest.mock("@nucleoidjs/webstorage", () => ({
+  storage: {
+    get: jest.fn(),
+    set: jest.fn(),
+  },
+}));
+
+global.window = Object.create(window);
+
+Object.defineProperty(window, "location", {
+  value: {
+    href: "http://nucleoid.com",
+  },
+  writable: true,
 });
 
-test("Check path used?", () => {
-  const paths = [
-    "/",
-    "/devices",
-    "/questions",
-    "/questions/reviews",
-    "/questions/reviews2",
-    "/questions/reviews/{rev3}",
-    "/questions/reviews/{rev3}/items",
-  ];
+describe("Path", () => {
+  describe("split", () => {
+    it("splits prefix and suffix", () => {
+      const { prefix, suffix } = Path.split("/questions/reviews/{rev3}/items");
+      expect(prefix).toEqual("/questions/reviews/{rev3}");
+      expect(suffix).toEqual("items");
+    });
+  });
 
-  const resultSameValue = Path.isUsed(
-    paths,
-    "/questions",
-    "reviews",
-    "reviews"
-  );
+  describe("isUsed", () => {
+    let paths;
 
-  const resultValueNull = Path.isUsed(paths, "/questions", "reviews", "");
+    beforeEach(() => {
+      paths = [
+        "/",
+        "/devices",
+        "/questions",
+        "/questions/reviews",
+        "/questions/reviews2",
+        "/questions/reviews/{rev3}",
+        "/questions/reviews/{rev3}/items",
+      ];
+    });
 
-  const resultValueUsed = Path.isUsed(
-    paths,
-    "/questions",
-    "reviews",
-    "reviews2"
-  );
+    it("returns false when the path is the same as the old path", () => {
+      const result = Path.isUsed(paths, "/questions", "reviews", "reviews");
+      expect(result).toEqual(false);
+    });
 
-  const resultValueUnUsed = Path.isUsed(
-    paths,
-    "/questions",
-    "reviews",
-    "invalid"
-  );
+    it("returns true when the new path is empty", () => {
+      const result = Path.isUsed(paths, "/questions", "reviews", "");
+      expect(result).toEqual(true);
+    });
 
-  expect(resultSameValue).toEqual(false);
-  expect(resultValueNull).toEqual(true);
-  expect(resultValueUsed).toEqual(true);
-  expect(resultValueUnUsed).toEqual(false);
+    it("returns true when the new path is already used", () => {
+      const result = Path.isUsed(paths, "/questions", "reviews", "reviews2");
+      expect(result).toEqual(true);
+    });
+
+    it("returns false when the new path is not used", () => {
+      const result = Path.isUsed(paths, "/questions", "reviews", "invalid");
+      expect(result).toEqual(false);
+    });
+  });
+
+  describe("getProjectId", () => {
+    it("returns the project id", () => {
+      window.location.pathname =
+        "/ide/f1f04060-1ea4-46fc-bbf9-fb69c1faca8b/api";
+      const id = Path.getProjectId();
+      expect(id).toEqual("f1f04060-1ea4-46fc-bbf9-fb69c1faca8b");
+    });
+  });
+
+  describe("getRecentProject", () => {
+    it.skip("returns the selected project in storage", () => {
+      const projectId = "f1f04060-1ea4-46fc-bbf9-fb69c1faca8b";
+
+      storage.set("ide", "selected", "project", projectId);
+
+      expect(storage.set).toHaveBeenCalledWith(
+        "ide",
+        "selected",
+        "project",
+        projectId
+      );
+
+      const recentProject = Path.getRecentProject();
+
+      expect(storage.get).toHaveBeenCalledWith("ide", "selected", "project");
+
+      expect(recentProject).toEqual(projectId);
+    });
+  });
+
+  it.skip("returns null when the selected project is not found", () => {
+    const storage = require("@nucleoidjs/webstorage");
+    storage.get.mockReturnValueOnce(null);
+
+    const recentProject = Path.getRecentProject();
+    expect(recentProject).toEqual(null);
+  });
+});
+
+describe("getMode", () => {
+  it("returns the mode from the URL", () => {
+    window.location.search = "?mode=local";
+    const mode = Path.getMode();
+    expect(mode).toEqual("local");
+  });
+
+  it("returns the mode from the project id", () => {
+    window.location.search = "";
+    window.location.pathname = "ide/f1f04060-1ea4-46fc-bbf9-fb69c1faca8b/api";
+    const mode = Path.getMode();
+    expect(mode).toEqual("cloud");
+  });
+
+  it("returns null when the mode is not found", () => {
+    window.location.search = "";
+    window.location.pathname = "ide/api";
+    const mode = Path.getMode();
+    expect(mode).toEqual(null);
+  });
 });
