d755b0cd93386b76b9f829d3ae427dbdfbc9fae7
Halil Ibrahim Cengel <halilibrahimcengel@icloud.com>
2024-04-09 13:54:23 +0300

    Refactor RouteManager for support multiple path

--- src/RouteManager.jsx
+++ src/RouteManager.jsx
@@ -1,30 +1,37 @@
 import React from "react";
 
-import { Navigate, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 const RouteManager = ({ routes }) => {
   return (
     <Routes>
       {routes.map((route, i) =>
         route.container ? (
-          <Route
-            key={i}
-            path={route.container?.path || "/"}
-            element={route.container.element}
-          >
-            {route.pages &&
-              route.pages.map((page, j) => (
-                <Route key={j} path={page.path} element={page.element} />
-              ))}
-            {route.rules &&
-              route.rules.map((rule, k) => (
-                <Route
-                  key={k}
-                  path={rule.path}
-                  element={<Navigate to={rule.to} />}
-                />
-              ))}
-          </Route>
+          Array.isArray(route.container.path) ? (
+            route.container.path.map((path, k) => (
+              <Route
+                key={`${i}-${k}`}
+                path={path}
+                element={route.container.element}
+              >
+                {route.pages &&
+                  route.pages.map((page, j) => (
+                    <Route key={j} path={page.path} element={page.element} />
+                  ))}
+              </Route>
+            ))
+          ) : (
+            <Route
+              key={i}
+              path={route.container.path}
+              element={route.container.element}
+            >
+              {route.pages &&
+                route.pages.map((page, j) => (
+                  <Route key={j} path={page.path} element={page.element} />
+                ))}
+            </Route>
+          )
         ) : (
           <Route key={i} path={route.path} element={route?.element} />
         )
