8066d48958fc73d06ecefd8a4fb03087b62abde2
Halil Ibrahim Cengel <halilibrahimcengel@icloud.com>
2024-04-16 00:08:00 +0300

    Create editor save tests for projects

---
+++ cypress/e2e/cloud-projects.cy.js
@@ -0,0 +1,232 @@
+describe("local project spec", () => {
+  beforeEach(() => {
+    cy.window().then((win) => {
+      win.localStorage.setItem("ide.landing", JSON.stringify({ level: 2 }));
+    });
+    cy.fixture("/GET/projects.json").then((projects) => {
+      cy.intercept("GET", "https://nuc.land/ide/api/projects", {
+        statusCode: 200,
+        body: projects,
+      });
+    });
+
+    cy.fixture("/GET/config.json").then((config) => {
+      cy.intercept("GET", "https://nucleoid.com/config", {
+        statusCode: 200,
+        body: config,
+      });
+    });
+
+    const cloudProjectId = "a166cc16-5c76-4aac-819e-118207a5dfa9";
+    let serviceId;
+
+    cy.visit(`http://localhost:5173/ide/${cloudProjectId}`);
+
+    cy.fixture("/GET/projects.json")
+      .then((projects) => {
+        const cloudProject = projects.find((p) => p.id === cloudProjectId);
+        cy.intercept(
+          "GET",
+          `https://nuc.land/ide/api/projects/${cloudProjectId}`,
+          {
+            statusCode: 200,
+            body: cloudProject,
+          }
+        );
+      })
+      .as("project");
+
+    cy.fixture("/GET/single-project-service.json")
+      .then((service) => {
+        cy.intercept(
+          "GET",
+          `https://nuc.land/ide/api/projects/${cloudProjectId}/services`,
+          {
+            statusCode: 200,
+            body: service,
+          }
+        );
+        serviceId = service[0].id;
+      })
+      .as("services");
+
+    cy.fixture("/GET/context.json")
+      .then((context) => {
+        cy.intercept(
+          "GET",
+          `https://nuc.land/ide/api/services/${serviceId}/context`,
+          {
+            statusCode: 200,
+            body: context,
+          }
+        );
+      })
+      .as("context");
+
+    cy.wait(["@project", "@services", "@context"]);
+  });
+
+  it("should save api editor changes", () => {
+    const cloudProjectId = "a166cc16-5c76-4aac-819e-118207a5dfa9";
+    const serviceId = "06843e12-bc10-4648-99dc-85ad4be1cd09";
+
+    const changedEditorValue = `function action(req: { params: { item: string } }): any {\n  const newItem = req.params.item;\n  return Item[newItem];`;
+
+    cy.url().should("include", `/${cloudProjectId}/api`);
+
+    cy.get('textarea[role="textbox"]').click();
+    cy.get('textarea[role="textbox"]').clear();
+    cy.get('textarea[role="textbox"]').type(changedEditorValue, {
+      parseSpecialCharSequences: false,
+    });
+
+    cy.fixture("/PUT/changed-context.json")
+      .then((context) => {
+        cy.intercept(
+          "PUT",
+          `https://nuc.land/ide/api/services/${serviceId}/context`,
+          {
+            statusCode: 200,
+            body: context,
+          }
+        );
+      })
+      .as("contextPut");
+
+    cy.fixture("/PUT/changed-context.json")
+      .then((context) => {
+        cy.intercept(
+          "GET",
+          `https://nuc.land/ide/api/services/${serviceId}/context`,
+          {
+            statusCode: 200,
+            body: context,
+          }
+        );
+      })
+      .as("contextGet");
+
+    cy.wait("@contextPut");
+
+    cy.reload().then(() => {
+      cy.wait("@contextGet");
+      cy.get('textarea[role="textbox"]')
+        .invoke("val")
+        .then((val) => {
+          expect(val).to.include("newItem");
+        });
+    });
+  });
+
+  it("should save functions editor changes", () => {
+    const cloudProjectId = "a166cc16-5c76-4aac-819e-118207a5dfa9";
+    const serviceId = "06843e12-bc10-4648-99dc-85ad4be1cd09";
+
+    cy.url().should("include", `/${cloudProjectId}/api`);
+
+    cy.getBySel("menu-Functions").click();
+
+    const changedEditorValue = `class NewOrder {\n      name: string;\n      barcode: string;\n      constructor(name: string, barcode: string) {\n  this.name = name;\nthis.barcode = barcode;\n`;
+
+    cy.url().should("include", `/${cloudProjectId}/functions`);
+
+    cy.get('textarea[role="textbox"]').click();
+    cy.get('textarea[role="textbox"]').clear();
+    cy.get('textarea[role="textbox"]').type(changedEditorValue, {
+      parseSpecialCharSequences: false,
+    });
+
+    cy.fixture("/PUT/changed-context.json")
+      .then((context) => {
+        cy.intercept(
+          "PUT",
+          `https://nuc.land/ide/api/services/${serviceId}/context`,
+          {
+            statusCode: 200,
+            body: context,
+          }
+        );
+      })
+      .as("contextPut");
+
+    cy.fixture("/PUT/changed-context.json")
+      .then((context) => {
+        cy.intercept(
+          "GET",
+          `https://nuc.land/ide/api/services/${serviceId}/context`,
+          {
+            statusCode: 200,
+            body: context,
+          }
+        );
+      })
+      .as("contextGet");
+
+    cy.wait("@contextPut");
+
+    cy.reload().then(() => {
+      cy.wait("@contextGet");
+      cy.get('textarea[role="textbox"]')
+        .invoke("val")
+        .then((val) => {
+          expect(val).to.include("NewOrder");
+        });
+    });
+  });
+
+  it("should save logic editor changes", () => {
+    const cloudProjectId = "a166cc16-5c76-4aac-819e-118207a5dfa9";
+    const serviceId = "06843e12-bc10-4648-99dc-85ad4be1cd09";
+
+    cy.url().should("include", `/${cloudProjectId}/api`);
+
+    cy.getBySel("menu-Logic").click();
+
+    const changedEditorValue = `$Human.mortal = true;\nplaton = new Human('Platon');\nplaton.mortal === true;`;
+
+    cy.url().should("include", `/${cloudProjectId}/logic`);
+
+    cy.get('textarea[role="textbox"]').click();
+    cy.get('textarea[role="textbox"]').clear();
+    cy.get('textarea[role="textbox"]').type(changedEditorValue, {
+      parseSpecialCharSequences: false,
+    });
+
+    cy.fixture("/PUT/changed-context.json")
+      .then((context) => {
+        cy.intercept(
+          "PUT",
+          `https://nuc.land/ide/api/services/${serviceId}/context`,
+          {
+            statusCode: 200,
+            body: context,
+          }
+        );
+      })
+      .as("contextPut");
+
+    cy.fixture("/PUT/changed-context.json")
+      .then((context) => {
+        cy.intercept(
+          "GET",
+          `https://nuc.land/ide/api/services/${serviceId}/context`,
+          {
+            statusCode: 200,
+            body: context,
+          }
+        );
+      })
+      .as("contextGet");
+
+    cy.wait("@contextPut");
+
+    cy.reload().then(() => {
+      cy.wait("@contextGet");
+      cy.get('textarea[role="textbox"]')
+        .invoke("val")
+        .then((val) => {
+          expect(val).to.include("platon");
+        });
+    });
+  });
+});
---
+++ cypress/e2e/local-projects.cy.js
@@ -0,0 +1,120 @@
+describe("local project spec", () => {
+  beforeEach(() => {
+    cy.window().then((win) => {
+      win.localStorage.setItem("ide.landing", JSON.stringify({ level: 2 }));
+    });
+    cy.fixture("/GET/projects.json").then((projects) => {
+      cy.intercept("GET", "https://nuc.land/ide/api/projects", {
+        statusCode: 200,
+        body: projects,
+      });
+    });
+
+    cy.fixture("/GET/config.json").then((config) => {
+      cy.intercept("GET", "https://nucleoid.com/config", {
+        statusCode: 200,
+        body: config,
+      });
+    });
+
+    const localProjectId = "3450f289-0fc5-45e9-9a4a-606c0a63cdfe";
+
+    cy.fixture("/LOCAL/project.json").then((project) => {
+      cy.storageSet(["ide", "projects", localProjectId], project);
+    });
+
+    cy.visit(`http://localhost:5173/ide/${localProjectId}/?mode=local`);
+  });
+
+  it("should save api editor changes in storage", () => {
+    const localProjectId = "3450f289-0fc5-45e9-9a4a-606c0a63cdfe";
+
+    const changedEditorValue = `function action(req: { params: { item: string } }): any {\n  const newItem = req.params.item;\n  return Item[newItem];`;
+    cy.url().should("include", `/${localProjectId}/api?mode=local`);
+
+    cy.get('textarea[role="textbox"]').click();
+    cy.get('textarea[role="textbox"]').clear();
+    cy.get('textarea[role="textbox"]').type(changedEditorValue, {
+      parseSpecialCharSequences: false,
+    });
+
+    cy.wait(1000);
+
+    cy.window()
+      .its("localStorage")
+      .then((localStorage) => {
+        const context = localStorage.getItem(
+          "ide.projects.3450f289-0fc5-45e9-9a4a-606c0a63cdfe"
+        );
+        expect(JSON.parse(context).api[3]["x-nuc-action"]).to.include(
+          "newItem"
+        );
+      });
+  });
+
+  it("should save functions editor changes", () => {
+    const localProjectId = "3450f289-0fc5-45e9-9a4a-606c0a63cdfe";
+
+    cy.url().should("include", `/${localProjectId}/api?mode=local`);
+
+    cy.getBySel("menu-Functions").click();
+
+    const changedEditorValue = `class NewOrder {\n      name: string;\n      barcode: string;\n      constructor(name: string, barcode: string) {\n  this.name = name;\nthis.barcode = barcode;\n`;
+
+    cy.url().should("include", `/${localProjectId}/functions?mode=local`);
+
+    cy.get("section").should("be.visible");
+    cy.get(".monaco-editor").should("be.visible");
+
+    cy.get('textarea[role="textbox"]').click();
+    cy.get('textarea[role="textbox"]').clear();
+    cy.get('textarea[role="textbox"]').type(changedEditorValue, {
+      parseSpecialCharSequences: false,
+    });
+
+    cy.wait(1000);
+
+    cy.window()
+      .its("localStorage")
+      .then((localStorage) => {
+        const context = localStorage.getItem(
+          "ide.projects.3450f289-0fc5-45e9-9a4a-606c0a63cdfe"
+        );
+
+        expect(JSON.parse(context).functions[0].definition).to.include(
+          "NewOrder"
+        );
+      });
+  });
+
+  it("should save logic editor changes", () => {
+    const localProjectId = "3450f289-0fc5-45e9-9a4a-606c0a63cdfe";
+
+    cy.url().should("include", `/${localProjectId}/api?mode=local`);
+
+    cy.getBySel("menu-Logic").click();
+
+    const changedEditorValue = `$Human.mortal = true;\nplaton = new Human('Platon');\nplaton.mortal === true;`;
+
+    cy.contains("All humans are mortal").click();
+
+    cy.get('textarea[role="textbox"]').click();
+    cy.get('textarea[role="textbox"]').clear();
+    cy.get('textarea[role="textbox"]').type(changedEditorValue, {
+      parseSpecialCharSequences: false,
+    });
+
+    cy.wait(1000);
+
+    cy.window()
+      .its("localStorage")
+      .then((localStorage) => {
+        const context = localStorage.getItem(
+          "ide.projects.3450f289-0fc5-45e9-9a4a-606c0a63cdfe"
+        );
+        expect(JSON.parse(context).declarations[0].definition).to.include(
+          "plato"
+        );
+      });
+  });
+});
