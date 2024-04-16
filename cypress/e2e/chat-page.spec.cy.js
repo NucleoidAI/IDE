07ef627e818ae0711d1929da8345a3b65acfb7f5
durulkoca <durulkoca@gmail.com>
2024-04-16 19:49:30 +0900

    Add seed data and update tests

--- cypress/e2e/chat-page.spec.cy.js
+++ cypress/e2e/chat-page.spec.cy.js
@@ -1,6 +1,57 @@
+import { chatData, chatDataKey, emptyChatData } from "../fixtures/seedData";
+
 describe("ChatWidget", () => {
   beforeEach(() => {
-    cy.visit("/ide/chat");
+    cy.visit("/ide/chat?clear=true");
+    cy.window().then((win) => {
+      win.localStorage.setItem(chatDataKey, JSON.stringify(chatData));
+    });
+    cy.visit(`/ide/chat/${chatData.id}`);
+  });
+
+  it("should display the initial chat messages from seed data", () => {
+    cy.get('[data-cy="message-box"]').should("have.length", 4);
+    cy.get('[data-cy="message-box"]')
+      .eq(0)
+      .within(() => {
+        cy.get('[data-cy="message-role"]').should("have.text", "USER");
+        cy.get('[data-cy="message-content"]').should(
+          "have.text",
+          "define a human"
+        );
+      });
+    cy.get('[data-cy="message-box"]')
+      .eq(1)
+      .within(() => {
+        cy.get('[data-cy="message-role"]').should("have.text", "ASSISTANT");
+        cy.get('[data-cy="message-content"]').should(
+          "have.text",
+          "Define a Human class with a name property and constructor"
+        );
+        cy.get('[data-cy="code-block"]').should("contain.text", "class Human");
+      });
+    cy.get('[data-cy="message-box"]')
+      .eq(2)
+      .within(() => {
+        cy.get('[data-cy="message-role"]').should("have.text", "USER");
+        cy.get('[data-cy="message-content"]').should(
+          "have.text",
+          "all humans are mortals"
+        );
+      });
+    cy.get('[data-cy="message-box"]')
+      .eq(3)
+      .within(() => {
+        cy.get('[data-cy="message-role"]').should("have.text", "ASSISTANT");
+        cy.get('[data-cy="message-content"]').should(
+          "have.text",
+          "Set the mortality property of all Human instances to true."
+        );
+        cy.get('[data-cy="code-block"]').should(
+          "contain.text",
+          "$Human.mortal = true;"
+        );
+      });
   });
 
   it("should send a message and receive a response without code", () => {
@@ -24,7 +75,7 @@ describe("ChatWidget", () => {
     cy.wait("@postMessage");
 
     cy.get('[data-cy="message-box"]')
-      .should("have.length.at.least", 2)
+      .should("have.length.at.least", 5)
       .last()
       .within(() => {
         cy.get('[data-cy="message-role"]').should("have.text", "ASSISTANT");
@@ -58,7 +109,7 @@ describe("ChatWidget", () => {
     cy.wait("@postMessage");
 
     cy.get('[data-cy="message-box"]')
-      .should("have.length.at.least", 2)
+      .should("have.length.at.least", 5)
       .last()
       .within(() => {
         cy.get('[data-cy="message-role"]').should("have.text", "ASSISTANT");
@@ -133,8 +184,8 @@ describe("ChatWidget", () => {
     cy.wait("@postMessage");
 
     cy.get('[data-cy="message-box"]')
-      .should("have.length.at.least", 4)
-      .eq(1)
+      .should("have.length.at.least", 8)
+      .eq(5)
       .within(() => {
         cy.get('[data-cy="message-role"]').should("have.text", "ASSISTANT");
         cy.get('[data-cy="message-content"]').should(
@@ -144,7 +195,7 @@ describe("ChatWidget", () => {
       });
 
     cy.get('[data-cy="message-box"]')
-      .eq(3)
+      .eq(7)
       .within(() => {
         cy.get('[data-cy="message-role"]').should("have.text", "ASSISTANT");
         cy.get('[data-cy="message-content"]').should(
@@ -156,6 +207,12 @@ describe("ChatWidget", () => {
   });
 
   it("should handle suggestions and update the overlay", () => {
+    cy.window().then((win) => {
+      win.localStorage.setItem(chatDataKey, JSON.stringify(emptyChatData));
+    });
+
+    cy.visit(`/ide/chat/${emptyChatData.id}`);
+
     cy.intercept("POST", "/ide/api/expert/chat/sessions/*", (req) => {
       if (req.body.content === "Option 1") {
         req.reply({
