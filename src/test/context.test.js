9d4616c6f76346f4f5283890f0151936e56651f8
Halil Ibrahim Cengel <halilibrahimcengel@icloud.com>
2024-04-19 20:16:27 +0300

    Fix jest test import issue

--- src/test/context.test.js
+++ src/test/context.test.js
@@ -1,10 +1,10 @@
-import State from "../state";
+import Context from "../context";
 import { contextReducer } from "../context/reducer";
 
 jest.mock("@nucleoidai/react-event", () => {});
 
 test("Resolve context with property", () => {
-  const state = contextReducer(State.init(), {
+  const state = contextReducer(Context.init(), {
     type: "SET_SELECTED_API",
     payload: { path: "/", method: "get" },
   });
@@ -12,6 +12,6 @@ test("Resolve context with property", () => {
 });
 
 test("Resolve context with invalid property", () => {
-  const state = contextReducer(State.init(), { type: "CLOSE_API_DIALOG" });
+  const state = contextReducer(Context.init(), { type: "CLOSE_API_DIALOG" });
   expect(state.get("pages.apix.dialog.open")).toEqual(undefined);
 });
