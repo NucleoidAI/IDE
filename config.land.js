ab049feec1efe466684195a83f883971f9651f22
Halil Ibrahim Cengel <halilibrahimcengel@icloud.com>
2024-04-23 14:29:01 +0300

    Standardize config files structure

--- config.land.js
+++ config.land.js
@@ -1,8 +1,15 @@
 const config = {
   name: "IDE",
   base: "/ide",
-  api: "http://localhost:3000",
+  api: "https://nuc.land/ide/api",
   expert: "https://nuc.land/ide/api/expert",
+  oauth: {
+    accessTokenUrl: "",
+    clientId: "",
+    redirectUri: "",
+    oauthUrl: "",
+  },
+  sandbox: "",
 };
 
 export default config;
