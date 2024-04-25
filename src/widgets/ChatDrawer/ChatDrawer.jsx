b9895857b73c98d343d64b56bd8f36bb29f12591
Halil Ibrahim Cengel <halilibrahimcengel@icloud.com>
2024-04-25 17:05:42 +0300

    Add open and close logic to ChatDrawer

--- src/widgets/ChatDrawer/ChatDrawer.jsx
+++ src/widgets/ChatDrawer/ChatDrawer.jsx
@@ -1,5 +1,22 @@
+import Drawer from "@mui/material/Drawer";
+import React from "react";
+import Stack from "@mui/material/Stack";
+import { useEvent } from "@nucleoidai/react-event";
+
 function ChatDrawer() {
-  return <></>;
+  const [chatDrawerOpened, publish] = useEvent("CHAT_DRAWER_OPENED", false);
+
+  const handleClose = () => {
+    publish("CHAT_DRAWER_OPENED", false);
+  };
+
+  return (
+    <React.Fragment>
+      <Drawer anchor="right" open={chatDrawerOpened} onClose={handleClose}>
+        <Stack width={"100%"}>i am chat</Stack>
+      </Drawer>
+    </React.Fragment>
+  );
 }
 
 export default ChatDrawer;
