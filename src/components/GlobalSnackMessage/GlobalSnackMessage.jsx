fba6381eb47441549d7fb84fae7a877f6ae82ac4
Halil Ibrahim Cengel <halilibrahimcengel@icloud.com>
2024-04-29 19:33:06 +0300

    Add auto hide functionality and remove status

--- src/components/GlobalSnackMessage/GlobalSnackMessage.jsx
+++ src/components/GlobalSnackMessage/GlobalSnackMessage.jsx
@@ -1,49 +1,52 @@
 import MuiAlert from "@mui/material/Alert";
-import React from "react";
 import Snackbar from "@mui/material/Snackbar";
 import { Stack } from "@mui/material";
+import { useEvent } from "@nucleoidai/react-event";
 
-import { publish, useEvent } from "@nucleoidai/react-event";
+import React, { useEffect, useState } from "react";
 
 const Alert = React.forwardRef(function Alert(props, ref) {
   return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
 });
 
 export default function GlobalSnackMessage() {
-  const [appMessage] = useEvent("APP_MESSAGE", {
-    status: false,
-    message: "",
-    severity: "success",
-  });
+  const [appMessage] = useEvent("APP_MESSAGE", null);
+  const [open, setOpen] = useState(false);
 
-  if (appMessage.status) {
-    return (
-      <Stack
+  useEffect(() => {
+    let timeoutId;
+    if (appMessage) {
+      setOpen(true);
+      timeoutId = setTimeout(() => {
+        setOpen(false);
+      }, 3000);
+    }
+    return () => clearTimeout(timeoutId);
+  }, [appMessage]);
+
+  return (
+    <Stack
+      sx={{
+        width: "500px",
+        height: "500px",
+        position: "relative",
+      }}
+    >
+      <Snackbar
         sx={{
-          width: "500px",
-          height: "500px",
-          position: "relative",
+          zIndex: 99999,
         }}
+        open={open}
+        anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
       >
-        <Snackbar
-          sx={{
-            zIndex: 99999,
-          }}
-          open={true}
-          autoHideDuration={6000}
-          anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+        <Alert
+          data-cy="global-snack-message"
+          severity={appMessage?.severity}
+          sx={{ width: "100%" }}
         >
-          <Alert
-            data-cy="global-snack-message"
-            severity={appMessage.severity}
-            sx={{ width: "100%" }}
-          >
-            {appMessage.message}
-          </Alert>
-        </Snackbar>
-      </Stack>
-    );
-  } else {
-    return null;
-  }
+          {appMessage?.message}
+        </Alert>
+      </Snackbar>
+    </Stack>
+  );
 }
