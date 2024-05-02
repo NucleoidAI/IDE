d28b9c295577b92d8df6d4337ece6e352dedff10
Halil Ibrahim Cengel <halilibrahimcengel@icloud.com>
2024-05-02 23:21:22 +0300

    Get base from config for vite and react-router

--- src/index.jsx
+++ src/index.jsx
@@ -4,15 +4,17 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import React from "react";
 import boot from "./boot";
+import config from "../config";
 import { createRoot } from "react-dom/client";
 
 boot.run();
+const { base } = config;
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
-  <BrowserRouter basename="ide">
+  <BrowserRouter basename={base}>
     <App />
   </BrowserRouter>
 );
