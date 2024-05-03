067bdc142005a91ebc21fd07d5b947efc2d1d7ca
Halil Ibrahim Cengel <halilibrahimcengel@icloud.com>
2024-05-03 04:18:37 +0300

    Fix invalidProjectId

--- cypress/e2e/projects-path.cy.js
+++ cypress/e2e/projects-path.cy.js
@@ -25,7 +25,7 @@ describe("Projects Path", () => {
     });
 
     it("redirects to error path if project id is invalid", () => {
-      const invalidProjectId = "1111111111";
+      const invalidProjectId = "f1f04060-1aa4-46fc-bbf9-fb69c1faca8b";
 
       cy.intercept(`/projects/${invalidProjectId}`);
       cy.intercept(`/projects/${invalidProjectId}/services`);
