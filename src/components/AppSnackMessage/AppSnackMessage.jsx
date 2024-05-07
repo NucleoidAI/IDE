e587ac5454eab0d9f489b0d72234d53ff3ade38c
Halil Ibrahim Cengel <halilibrahimcengel@icloud.com>
2024-05-07 17:42:56 +0300

    Rename GlobalSnackMessage to AppSnackMessage

--- src/components/AppSnackMessage/AppSnackMessage.jsx
+++ src/components/AppSnackMessage/AppSnackMessage.jsx
@@ -9,7 +9,7 @@ const Alert = React.forwardRef(function Alert(props, ref) {
   return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
 });
 
-export default function GlobalSnackMessage() {
+export default function AppSnackMessage() {
   const [appMessage] = useEvent("APP_MESSAGE", null);
   const [open, setOpen] = useState(false);
 
