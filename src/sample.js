9b82389c9680a7e376a4ef47b1c3f763c08e7615
Can Mingir <canmingir@gmail.com>
2024-05-09 20:40:39 -0400

    Restore sample

--- src/sample.js
+++ src/sample.js
@@ -21,25 +21,23 @@ export const api = [
     response: {
       type: "OPENAPI",
       schema: {
-        name: "User",
+        name: "Hello",
         type: "object",
         properties: [
           {
-            name: "name",
+            name: "message",
             type: "string",
-          },
-          {
-            name: "age",
-            type: "number",
           },
         ],
       },
     },
-    summary: "Get all users",
-    description: "Get all users",
-    action: `function action(req: any): { message: string } {
-        return { message: "Hello World" };
-      }`,
+    summary: "Hello World",
+    description: "Hello World",
+    action: `
+function action(req) {
+  return { message: "Hello World" };
+}
+`,
   },
   {
     path: "/items",
@@ -65,12 +63,14 @@ export const api = [
         ],
       },
     },
-    summary: "Get item by name",
-    description: "Get item by name",
-    action: `function action(req: { query: { name: string } }): any {
-        const name = req.query.name;
-        return (Item as any).filter(item => item.name === name);
-      }`,
+    summary: "List items by name",
+    description: "List items by name",
+    action: `
+function action(req) {
+  const name = req.query.name;
+  return Item.filter(item => item.name === name);
+}
+`,
   },
   {
     path: "/items",
@@ -106,28 +106,74 @@ export const api = [
     },
     summary: "Create new item",
     description: "Create new item",
-    action: `function action(req: { body: { name: string, barcode: string } }): any {
-        const name = req.body.name;
-        const barcode = req.body.barcode;
-        const check = (Item as any).find(i => i.barcode === barcode);
-        if(check) {
-          throw "DUPLICATE_BARCODE";
-        }
-        return new Item(name, barcode);
-      }`,
+    action: `
+function action(req) {
+  const name = req.body.name;
+  const barcode = req.body.barcode;
+
+  const check = Item.find(i => i.barcode === barcode);
+
+  if(check) {
+    throw "DUPLICATE_BARCODE";
+  }
+
+  return new Item(name, barcode);
+}
+`,
   },
   {
-    path: "/items/{item}",
+    path: "/items/{itemId}",
     method: "GET",
     params: [
       {
-        name: "item",
+        name: "itemId",
         in: "path",
         type: "string",
         required: true,
-        description: "item",
+        description: "itemId",
       },
     ],
+    response: {
+      type: "OPENAPI",
+      schema: {
+        type: "object",
+        properties: [
+          {
+            type: "ref",
+            name: "itemId",
+            ref: "Item",
+          },
+        ],
+      },
+    },
+    summary: "Get item by id",
+    description: "Get item by id",
+    action: `
+function action(req) {
+  const itemId = req.params.itemId;
+  return Item[itemId];
+}
+`,
+  },
+  {
+    path: "/items/{itemId}",
+    method: "PUT",
+    request: {
+      type: "OPENAPI",
+      schema: {
+        type: "object",
+        properties: [
+          {
+            name: "name",
+            type: "string",
+          },
+          {
+            name: "barcode",
+            type: "string",
+          },
+        ],
+      },
+    },
     response: {
       type: "OPENAPI",
       schema: {
@@ -141,40 +187,233 @@ export const api = [
         ],
       },
     },
-    summary: "Get item by id",
-    description: "Get item by id",
+    summary: "Update item by id",
+    description: "Update item by id",
+    action: `
+function action(req) {
+  const item = Item[req.params.item];
+  const name = req.body.name;
+  const barcode = req.body.barcode;
 
-    action: `function action(req: { params: { item: string } }): any {
-        const item = req.params.item;
-        return Item[item];
-      }`,
+  if (!item) {
+    return;
+  }
+
+  const check = Item.find(
+    i => i.barcode === barcode && i.barcode !== item.barcode
+  );
+
+  if (check) {
+    throw "DUPLICATE_BARCODE";
+  }
+
+  item.name = name;
+  item.barcode = barcode;
+
+  return item;
+}
+`,
   },
-];
-
-export const types = [
   {
-    name: "Item",
-    type: "OPENAPI",
-    schema: {
-      name: "Item",
-      type: "object",
-      properties: [
-        {
-          type: "string",
-          name: "id",
-        },
-        {
-          type: "string",
-          name: "name",
-        },
-        {
-          type: "string",
-          name: "barcode",
-        },
-      ],
+    path: "/items/{itemId}",
+    method: "DEL",
+    params: [
+      {
+        name: "itemId",
+        in: "path",
+        type: "string",
+        required: true,
+        description: "itemId",
+      },
+    ],
+    summary: "Delete item by id",
+    description: "Delete item by id",
+    action: `
+function action(req) {
+  const item = req.params.item;
+  delete Item[item];
+}
+`,
+  },
+  {
+    path: "/orders",
+    method: "GET",
+    params: [],
+    response: {
+      type: "OPENAPI",
+      schema: {
+        type: "object",
+        properties: [
+          {
+            type: "ref",
+            name: "itemId",
+            ref: "Item",
+          },
+        ],
+      },
     },
+    summary: "List orders",
+    description: "List orders",
+    action: `
+function action(req) {
+  return Order;
+}
+`,
+  },
+  {
+    path: "/orders",
+    method: "POST",
+    params: [],
+    request: {
+      type: "OPENAPI",
+      schema: {
+        type: "object",
+        properties: [
+          {
+            name: "item",
+            type: "string",
+          },
+          {
+            name: "qty",
+            type: "integer",
+          },
+        ],
+      },
+    },
+    response: {
+      type: "OPENAPI",
+      schema: {
+        type: "object",
+        properties: [
+          {
+            type: "ref",
+            name: "itemId",
+            ref: "Item",
+          },
+        ],
+      },
+    },
+    summary: "Create order",
+    description: "Create order",
+    action: `
+function action(req) {
+  const item = Item[req.body.item];
+  const qty = req.body.qty;
+
+  if (!item) {
+    throw "INVALID_ITEM";
+  }
+
+  return new Order(item, qty);
+}
+`,
+  },
+  {
+    path: "/orders/{orderId}",
+    method: "GET",
+    params: [
+      {
+        name: "orderId",
+        in: "path",
+        type: "string",
+        required: true,
+        description: "orderId",
+      },
+    ],
+    response: {
+      type: "OPENAPI",
+      schema: {
+        type: "object",
+        properties: [
+          {
+            type: "ref",
+            name: "itemId",
+            ref: "Item",
+          },
+        ],
+      },
+    },
+    summary: "Get order by id",
+    description: "Get order by id",
+    action: `
+function action(req) {
+  const itemId = req.params.itemId;
+  return Item[itemId];
+}
+`,
+  },
+  {
+    path: "/orders/{orderId}",
+    method: "PUT",
+    params: [
+      {
+        name: "orderId",
+        in: "path",
+        type: "string",
+        required: true,
+        description: "orderId",
+      },
+    ],
+    response: {
+      type: "OPENAPI",
+      schema: {
+        type: "object",
+        properties: [
+          {
+            type: "ref",
+            name: "itemId",
+            ref: "Item",
+          },
+        ],
+      },
+    },
+    summary: "Update order by id",
+    description: "Update order by id",
+    action: `
+function action(req) {
+  const order = Order[req.params.order];
+  const item = Item[req.body.item];
+  const qty = req.body.qty;
+
+  if (!order) {
+    return;
+  }
+
+  if (!item) {
+    throw "INVALID_ITEM";
+  }
+
+  order.item = item;
+  order.qty = qty;
+
+  return order;
+}
+`,
+  },
+  {
+    path: "/orders/{orderId}",
+    method: "DEL",
+    params: [
+      {
+        name: "orderId",
+        in: "path",
+        type: "string",
+        required: true,
+        description: "orderId",
+      },
+    ],
+    summary: "Delete order by id",
+    description: "Delete order by id",
+    action: `
+function action(req) {
+  const item = req.params.item;
+  delete Item[item];
+}
+`,
   },
 ];
+
+export const types = [];
 
 export const functions = [
   {
@@ -182,11 +421,12 @@ export const functions = [
     params: ["name: string", "barcode: string"],
     type: "CLASS",
     definition: `class Order {
-      name: string;
-      barcode: string;
-      constructor(name: string, barcode: string) {
-        this.name = name;
-        this.barcode = barcode;
+      item: Item;
+      qty: number;
+
+      constructor(item: Item, qty: number) {
+        this.item = item;
+        this.qty = qty;
       }
     }`,
   },
@@ -197,19 +437,7 @@ export const functions = [
     definition: `class Item {
       name: string;
       barcode: string;
-      constructor(name: string, barcode: string) {
-        this.name = name;
-        this.barcode = barcode;
-      }
-    }`,
-  },
-  {
-    path: "/Human",
-    params: ["name: string", "barcode: string"],
-    type: "CLASS",
-    definition: `class Human {
-      name: string;
-      barcode: string;
+
       constructor(name: string, barcode: string) {
         this.name = name;
         this.barcode = barcode;
