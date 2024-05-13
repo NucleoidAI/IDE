f25e4b1673cc9cf39703e1c24cd4a64a28ef54f6
Halil Ibrahim Cengel <halilibrahimcengel@icloud.com>
2024-05-13 18:02:17 +0300

    Add expert interceptor

---
+++ src/http/expert.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+import axiosRetry from "axios-retry";
+import config from "../../config.js";
+import { publish } from "@nucleoidai/react-event";
+
+const instance = axios.create({
+  baseURL: config.expert,
+});
+
+axiosRetry(instance, { retries: 0 });
+axios.defaults.headers.common["Content-Type"] = "application/json";
+
+instance.interceptors.response.use(
+  (res) => res,
+  (err) => {
+    if (err.response.status === 500) {
+      publish("GLOBAL_MESSAGE", {
+        status: true,
+        message: err.message,
+        severity: "error",
+      });
+    }
+
+    return Promise.reject(err);
+  }
+);
+
+export default instance;
