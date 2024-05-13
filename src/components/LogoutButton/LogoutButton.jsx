be7a1ec909e45b802c539e7ccc57267c823bcd24
Halil Ibrahim Cengel <halilibrahimcengel@icloud.com>
2024-05-13 17:30:36 +0300

    Add logout functionality to LogoutButton

--- src/components/LogoutButton/LogoutButton.jsx
+++ src/components/LogoutButton/LogoutButton.jsx
@@ -1,9 +1,12 @@
 import Button from "@mui/material/Button";
 import LogoutIcon from "@mui/icons-material/Logout";
+import { publish } from "@nucleoidai/react-event";
+import { storage } from "@nucleoidjs/webstorage";
 
 function LogoutButton() {
   const handleLogout = () => {
-    console.log("Logout");
+    storage.remove("oauth.token");
+    publish("USER_LOGGED_OUT");
   };
   return (
     <Button variant="gray" onClick={handleLogout} startIcon={<LogoutIcon />}>
