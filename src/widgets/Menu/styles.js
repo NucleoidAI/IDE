d4273bfb67f713496ab5f93aab0ff744742d2a8a
Ebubekir Yılmaz <ebu1205ed@gmail.com>
2024-05-23 08:19:38 +0300

    Add styles to fix button animation

--- src/widgets/Menu/styles.js
+++ src/widgets/Menu/styles.js
@@ -15,8 +15,15 @@ const styles = {
     "&:hover,&:focus": {
       color: "rgba(255, 255, 255, 1)",
     },
+    "& .MuiTouchRipple-root": {
+      backgroundColor: "rgba(255, 255, 255, 0.7)",
+      opacity: 0,
+      transition: "opacity .4s",
+    },
+    "&:active .MuiTouchRipple-root": {
+      opacity: 0.6,
+    },
   },
-
   listItemIconSmall: {
     color: "custom.grey",
     width: "100%",
