b4fd8f928df422731edc9207129623cc81e1538c
Ebubekir Yılmaz <ebu1205ed@gmail.com>
2024-05-27 11:07:24 +0300

    Remove try catch in usechat

--- src/widgets/Chat/useChat.jsx
+++ src/widgets/Chat/useChat.jsx
@@ -23,38 +23,29 @@ const useChat = () => {
   }, [selectedChat]);
 
   const sendMessage = async (message) => {
-    try {
-      const { data } = await expert.post(`/chat/sessions/${chat.id}`, {
-        role: "USER",
-        content: message,
-      });
+    const { data } = await expert.post(`/chat/sessions/${chat.id}`, {
+      role: "USER",
+      content: message,
+    });
 
-      const assistantMessage = {
-        role: "assistant",
-        ...data,
-      };
+    const assistantMessage = {
+      role: "assistant",
+      ...data,
+    };
 
-      const updatedMessages = [
-        ...chat.messages,
-        { role: "USER", content: message },
-        assistantMessage,
-      ];
-      const updatedChat = { ...chat, messages: updatedMessages };
-      localStorage.setItem(
-        `ide.chat.sessions.${chat.id}`,
-        JSON.stringify(updatedChat)
-      );
+    const updatedMessages = [
+      ...chat.messages,
+      { role: "USER", content: message },
+      assistantMessage,
+    ];
+    const updatedChat = { ...chat, messages: updatedMessages };
+    localStorage.setItem(
+      `ide.chat.sessions.${chat.id}`,
+      JSON.stringify(updatedChat)
+    );
 
-      publish("CHAT_MESSAGE_RESPONDED", updatedChat);
-      setChat(updatedChat);
-    } catch ({ response }) {
-      publish("EXPERT_ERROR_OCCURRED", {
-        chatId: chat.id,
-        status: true,
-        type: response.data.type,
-        content: response.data.content,
-      });
-    }
+    publish("CHAT_MESSAGE_RESPONDED", updatedChat);
+    setChat(updatedChat);
   };
 
   const convertChat = (chatToConvert) => {
