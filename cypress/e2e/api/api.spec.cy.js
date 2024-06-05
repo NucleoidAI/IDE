85a745a413065e40f803636be961c3fe809d51c5
durulkoca <durulkoca@gmail.com>
2024-06-05 15:07:08 +0300

    Separate one api test into two and move waitevent to beforeEach

--- cypress/e2e/api/api.spec.cy.js
+++ cypress/e2e/api/api.spec.cy.js
@@ -7,71 +7,64 @@ describe("API Page", () => {
     cy.get("@projectId").then((projectId) => {
       cy.visit(`/${projectId}/api?mode=local`);
     });
+
+    cy.waitEvent("CONTAINER_LOADED");
   });
 
   it("opens APIDialog for given method and resource with right click", () => {
-    cy.waitEvent("CONTAINER_LOADED");
     cy.openAPIDialog("edit");
 
     cy.getBySel("method-text").should("have.text", "GET");
     cy.getBySel("path-text").should("have.text", "/ ");
   });
 
-  it("deletes method and resources with right click", () => {
-    cy.get("@projectId").then((projectId) => {
-      cy.visit(`/${projectId}/api?mode=local`);
-      cy.waitEvent("CONTAINER_LOADED");
-      cy.get('[data-cy^="method-"]').then(($methods) => {
-        const methodCount = $methods.length;
-        if (methodCount >= 2) {
-          cy.get('[data-cy^="method-"]')
-            .eq(1)
-            .then(($method) => {
-              const methodName = $method.attr("data-cy");
-              cy.wrap($method).rightclick();
-              cy.getBySel("delete-method-button").click();
-              cy.getBySel("delete-method-confirm-button").click();
-              cy.get('[data-cy^="method-"]').should(
-                "have.length",
-                methodCount - 1
-              );
-              cy.get(`[data-cy="${methodName}"]`).should("not.exist");
-            });
-        }
-      });
+  it("deletes methods with right click", () => {
+    cy.get('[data-cy^="method-"]').then(($methods) => {
+      const methodCount = $methods.length;
+      if (methodCount >= 2) {
+        cy.get('[data-cy^="method-"]')
+          .eq(1)
+          .then(($method) => {
+            const methodName = $method.attr("data-cy");
+            cy.wrap($method).rightclick();
+            cy.getBySel("delete-method-button").click();
+            cy.getBySel("delete-method-confirm-button").click();
+            cy.get('[data-cy^="method-"]').should(
+              "have.length",
+              methodCount - 1
+            );
+            cy.get(`[data-cy="${methodName}"]`).should("not.exist");
+          });
+      }
     });
+  });
 
-    cy.get("@projectId").then((projectId) => {
-      cy.visit(`/${projectId}/api?mode=local`);
-      cy.waitEvent("CONTAINER_LOADED");
-      cy.get('[data-cy^="path-"]').then(($paths) => {
-        const pathCount = $paths.length;
-        if (pathCount >= 2) {
-          cy.get('[data-cy^="path-"]')
-            .eq(1)
-            .then(($path) => {
-              const pathName = $path.attr("data-cy");
-              cy.wrap($path).rightclick();
-              cy.getBySel("delete-resource").click();
-              cy.getBySel("delete-resource-confirm-button").click();
-              cy.get('[data-cy^="path-"]').should(($updatedPaths) => {
-                expect($updatedPaths.length).to.be.lessThan(pathCount);
-              });
-              cy.get(`[data-cy="${pathName}"]`).should("not.exist");
+  it("deletes resources with right click", () => {
+    cy.get('[data-cy^="path-"]').then(($paths) => {
+      const pathCount = $paths.length;
+      if (pathCount >= 2) {
+        cy.get('[data-cy^="path-"]')
+          .eq(1)
+          .then(($path) => {
+            const pathName = $path.attr("data-cy");
+            cy.wrap($path).rightclick();
+            cy.getBySel("delete-resource").click();
+            cy.getBySel("delete-resource-confirm-button").click();
+            cy.get('[data-cy^="path-"]').should(($updatedPaths) => {
+              expect($updatedPaths.length).to.be.lessThan(pathCount);
             });
-        }
-      });
+            cy.get(`[data-cy="${pathName}"]`).should("not.exist");
+          });
+      }
     });
   });
 
   it("displays schema view of request and response in APISetting", () => {
-    cy.waitEvent("CONTAINER_LOADED");
     cy.getBySel("request-schema").should("exist");
     cy.getBySel("response-schema").should("exist");
   });
 
   it("updates summary and description", () => {
-    cy.waitEvent("CONTAINER_LOADED");
     cy.get('[data-cy^="method-"]').first().click();
 
     const newSummary = "New Summary";
@@ -92,7 +85,6 @@ describe("API Page", () => {
   });
 
   it("prevents deleting root resource", () => {
-    cy.waitEvent("CONTAINER_LOADED");
     cy.get('[data-cy^="path-"]').then(($paths) => {
       const pathCount = $paths.length;
       if (pathCount >= 2) {
