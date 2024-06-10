b4f72f1f4704a861820c3469cf2f3422537b45cf
Halil Ibrahim Cengel <halilibrahimcengel@icloud.com>
2024-06-10 19:01:46 +0300

    Create logic-page spec and add test by mode

---
+++ cypress/e2e/logic-page.spec.cy.js
@@ -0,0 +1,66 @@
+describe("Local Mode", () => {
+  beforeEach(() => {
+    cy.setup("IDE", "SEED", "LOCAL");
+    cy.fixture("PROJECTS/LOCAL/project.json").as("project");
+    cy.wrap("3450f289-0fc5-45e9-9a4a-606c0a63cdfe").as("projectId");
+  });
+
+  it("saves changes in logic editor", () => {
+    cy.get("@projectId").then((projectId) => {
+      cy.visit(`/${projectId}/logic?mode=local`);
+    });
+
+    cy.waitEvent("CONTAINER_LOADED").then(() => {
+      const changedEditorValue = `$Human.mortal = true;\nplaton = new Human('Platon');\nplaton.mortal === true;`;
+
+      cy.typeEditor(changedEditorValue);
+
+      cy.get("@projectId").then((projectId) => {
+        cy.storageGet(`ide.context.${projectId}`).then((project) => {
+          cy.normalizeString(
+            project.specification.declarations[0].definition
+          ).then((normalizedDefinition) => {
+            cy.normalizeString(changedEditorValue).then(
+              (normalizedChangedEditorValue) => {
+                expect(normalizedDefinition).to.include(
+                  normalizedChangedEditorValue
+                );
+              }
+            );
+          });
+        });
+      });
+    });
+  });
+});
+
+describe("Cloud Mode", () => {
+  beforeEach(() => {
+    cy.setup("IDE", "SEED", "CLOUD");
+
+    cy.wrap("a166cc16-5c76-4aac-819e-118207a5dfa9").as("projectId");
+    cy.wrap("06843e12-bc10-4648-99dc-85ad4be1cd09").as("serviceId");
+
+    cy.get("@serviceId").then((serviceId) => {
+      cy.saveContextIntercept(serviceId).as("saveContext");
+    });
+  });
+
+  it("saves changes in logic editor", () => {
+    cy.get("@projectId").then((projectId) => {
+      cy.visit(`/${projectId}/logic`);
+    });
+
+    cy.waitEvent("CONTAINER_LOADED");
+
+    const changedEditorValue = `$Human.mortal = true;\nplaton = new Human('Platon');\nplaton.mortal === true;`;
+
+    cy.typeEditor(changedEditorValue);
+
+    cy.reload().then(() => {
+      cy.waitEvent("CONTAINER_LOADED");
+
+      cy.checkEditorValue("platon");
+    });
+  });
+});
