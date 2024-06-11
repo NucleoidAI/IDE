55a48bf6e14e4944e32b9b2b448a53d071a30141
durulkoca <durulkoca@gmail.com>
2024-06-11 15:23:57 +0300

    Refactor adapter tests to check for key parameters instead of full expected schema

--- src/test/adapter.test.js
+++ src/test/adapter.test.js
@@ -3,10 +3,6 @@ import { toOpenApi } from "../adapters/openapi/adapter";
 import {
   apiData,
   declarationsData,
-  openApiData as expectedFullOpenApi,
-  expectedNestedResourceNestedTypesOpenApi,
-  expectedNestedResourceSingleTypeOpenApi,
-  expectedSingleResourceSingleTypeOpenApi,
   functionsData,
   nestedResourceApiData,
   nestedTypeData,
@@ -16,6 +12,19 @@ import {
 } from "./adapterTestData";
 
 describe("OpenAPI Adapter", () => {
+  const checkResponseSchema = (response) => {
+    expect(response).toHaveProperty("description");
+    expect(response.content).toHaveProperty("application/json");
+    expect(response.content["application/json"]).toHaveProperty("schema");
+  };
+
+  const checkParameterSchema = (param) => {
+    expect(param).toHaveProperty("name");
+    expect(param).toHaveProperty("in");
+    expect(param).toHaveProperty("required");
+    expect(param).toHaveProperty("schema");
+  };
+
   test("should convert API definition, types, functions, and declarations to OpenAPI specification", () => {
     const actualOpenApi = toOpenApi({
       api: apiData,
@@ -24,7 +33,30 @@ describe("OpenAPI Adapter", () => {
       declarations: declarationsData,
     });
 
-    expect(actualOpenApi).toEqual(expectedFullOpenApi);
+    expect(Object.keys(actualOpenApi.openapi.paths).length).toBeGreaterThan(0);
+
+    Object.values(actualOpenApi.openapi.paths).forEach((path) => {
+      Object.values(path).forEach((method) => {
+        expect(method).toHaveProperty("summary");
+        expect(method).toHaveProperty("description");
+        expect(method).toHaveProperty("responses");
+        Object.values(method.responses).forEach(checkResponseSchema);
+        method.parameters?.forEach(checkParameterSchema);
+      });
+    });
+
+    expect(
+      Object.keys(actualOpenApi.openapi.components.schemas).length
+    ).toBeGreaterThan(0);
+
+    expect(actualOpenApi.openapi).toHaveProperty(
+      "x-nuc-functions",
+      functionsData
+    );
+    expect(actualOpenApi.openapi).toHaveProperty(
+      "x-nuc-declarations",
+      declarationsData
+    );
   });
 
   test("should convert API and types (Single resource, single type)", () => {
@@ -35,7 +67,21 @@ describe("OpenAPI Adapter", () => {
       declarations: declarationsData,
     });
 
-    expect(actualOpenApi).toEqual(expectedSingleResourceSingleTypeOpenApi);
+    expect(Object.keys(actualOpenApi.openapi.paths).length).toBeGreaterThan(0);
+
+    Object.values(actualOpenApi.openapi.paths).forEach((path) => {
+      Object.values(path).forEach((method) => {
+        expect(method).toHaveProperty("summary");
+        expect(method).toHaveProperty("description");
+        expect(method).toHaveProperty("responses");
+        Object.values(method.responses).forEach(checkResponseSchema);
+        method.parameters?.forEach(checkParameterSchema);
+      });
+    });
+
+    expect(
+      Object.keys(actualOpenApi.openapi.components.schemas).length
+    ).toBeGreaterThan(0);
   });
 
   test("should convert nested API and types (Nested resource and single type)", () => {
@@ -46,7 +92,21 @@ describe("OpenAPI Adapter", () => {
       declarations: declarationsData,
     });
 
-    expect(actualOpenApi).toEqual(expectedNestedResourceSingleTypeOpenApi);
+    expect(Object.keys(actualOpenApi.openapi.paths).length).toBeGreaterThan(0);
+
+    Object.values(actualOpenApi.openapi.paths).forEach((path) => {
+      Object.values(path).forEach((method) => {
+        expect(method).toHaveProperty("summary");
+        expect(method).toHaveProperty("description");
+        expect(method).toHaveProperty("responses");
+        Object.values(method.responses).forEach(checkResponseSchema);
+        method.parameters?.forEach(checkParameterSchema);
+      });
+    });
+
+    expect(
+      Object.keys(actualOpenApi.openapi.components.schemas).length
+    ).toBeGreaterThan(0);
   });
 
   test("should convert nested API and nested types (Nested resource and nested types)", () => {
@@ -57,7 +117,21 @@ describe("OpenAPI Adapter", () => {
       declarations: declarationsData,
     });
 
-    expect(actualOpenApi).toEqual(expectedNestedResourceNestedTypesOpenApi);
+    expect(Object.keys(actualOpenApi.openapi.paths).length).toBeGreaterThan(0);
+
+    Object.values(actualOpenApi.openapi.paths).forEach((path) => {
+      Object.values(path).forEach((method) => {
+        expect(method).toHaveProperty("summary");
+        expect(method).toHaveProperty("description");
+        expect(method).toHaveProperty("responses");
+        Object.values(method.responses).forEach(checkResponseSchema);
+        method.parameters?.forEach(checkParameterSchema);
+      });
+    });
+
+    expect(
+      Object.keys(actualOpenApi.openapi.components.schemas).length
+    ).toBeGreaterThan(0);
   });
 
   test("should handle empty API definition", () => {
