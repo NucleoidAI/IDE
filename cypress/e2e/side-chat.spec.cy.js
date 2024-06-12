912b797d6443b891fb44bcbcb96c01c1f74201a0
Ebubekir Yılmaz <ebu1205ed@gmail.com>
2024-06-12 10:34:03 +0300

    Wrap describes in Side Chat

--- cypress/e2e/side-chat.spec.cy.js
+++ cypress/e2e/side-chat.spec.cy.js
@@ -1,69 +1,71 @@
-describe("created project by default", () => {
-  beforeEach(() => {
-    cy.setup("IDE", "SEED", "LOCAL");
-    cy.fixture("PROJECTS/LOCAL/project.json").as("project");
-    cy.wrap("3450f289-0fc5-45e9-9a4a-606c0a63cdfe").as("projectId");
-  });
-  it("should create and save a new session when side chat is opened", () => {
-    cy.get("@projectId").then((projectId) => {
-      cy.visit(`/${projectId}/api?mode=local`);
+describe("Side Chat", () => {
+  describe("created project by default", () => {
+    beforeEach(() => {
+      cy.setup("IDE", "SEED", "LOCAL");
+      cy.fixture("PROJECTS/LOCAL/project.json").as("project");
+      cy.wrap("3450f289-0fc5-45e9-9a4a-606c0a63cdfe").as("projectId");
     });
+    it("should create and save a new session when side chat is opened", () => {
+      cy.get("@projectId").then((projectId) => {
+        cy.visit(`/${projectId}/api?mode=local`);
+      });
 
-    cy.waitEvent("CONTAINER_LOADED").then(() => {
-      cy.getBySel("side-chat-button").click();
+      cy.waitEvent("CONTAINER_LOADED").then(() => {
+        cy.getBySel("side-chat-button").click();
 
-      cy.getBySel("chat-welcome-message").should("to.visible");
+        cy.getBySel("chat-welcome-message").should("to.visible");
 
-      cy.sendMessage("hello", "MESSAGES/hello");
+        cy.sendMessage("hello", "MESSAGES/hello");
 
-      const expectedRespnse =
-        '"Nucleoid Chat" is a platform specifically designed for posing and discussing formal logic questions. Nucleoid Runtime is a software system that executes and manages logical rules and inferences.';
+        const expectedRespnse =
+          '"Nucleoid Chat" is a platform specifically designed for posing and discussing formal logic questions. Nucleoid Runtime is a software system that executes and manages logical rules and inferences.';
 
-      cy.checkMessageResponse("ASSISTANT", expectedRespnse, 2, false, "last");
+        cy.checkMessageResponse("ASSISTANT", expectedRespnse, 2, false, "last");
 
-      cy.get("@projectId").then((id) => {
-        cy.storageGet(`ide.chat.sessions.${id}`).then((session) => {
-          expect(session.messages[0]).to.deep.equal({
-            role: "USER",
-            content: "hello",
-          });
-          expect(session.messages[1]).to.deep.equal({
-            role: "ASSISTANT",
-            type: "EXCLAMATORY",
-            content: expectedRespnse,
+        cy.get("@projectId").then((id) => {
+          cy.storageGet(`ide.chat.sessions.${id}`).then((session) => {
+            expect(session.messages[0]).to.deep.equal({
+              role: "USER",
+              content: "hello",
+            });
+            expect(session.messages[1]).to.deep.equal({
+              role: "ASSISTANT",
+              type: "EXCLAMATORY",
+              content: expectedRespnse,
+            });
           });
         });
       });
     });
   });
-});
 
-describe("created project by chat", () => {
-  beforeEach(() => {
-    cy.setup("IDE", "SEED", "LOCAL");
-    cy.fixture("PROJECTS/LOCAL/project.json").as("project");
-    cy.wrap("3450f289-0fc5-45e9-9a4a-606c0a63cdfe").as("projectId");
-    cy.fixture("CHAT/chat-data.json").then((session) => {
-      cy.get("@projectId").then((id) => {
-        session.id = id;
-        cy.storageSet(`ide.chat.sessions.${id}`, session);
-        cy.visit(`/${id}/api?mode=local`);
+  describe("created project by chat", () => {
+    beforeEach(() => {
+      cy.setup("IDE", "SEED", "LOCAL");
+      cy.fixture("PROJECTS/LOCAL/project.json").as("project");
+      cy.wrap("3450f289-0fc5-45e9-9a4a-606c0a63cdfe").as("projectId");
+      cy.fixture("CHAT/chat-data.json").then((session) => {
+        cy.get("@projectId").then((id) => {
+          session.id = id;
+          cy.storageSet(`ide.chat.sessions.${id}`, session);
+          cy.visit(`/${id}/api?mode=local`);
+        });
       });
     });
-  });
-  it("should load old messages when side chat is opened", () => {
-    cy.waitEvent("CONTAINER_LOADED").then(() => {
-      cy.getBySel("side-chat-button").click();
+    it("should load old messages when side chat is opened", () => {
+      cy.waitEvent("CONTAINER_LOADED").then(() => {
+        cy.getBySel("side-chat-button").click();
 
-      cy.getBySel("chat-welcome-message").should("to.not.exist");
+        cy.getBySel("chat-welcome-message").should("to.not.exist");
 
-      cy.checkMessageResponse(
-        "ASSISTANT",
-        "Set the mortality property of all Human instances to true.",
-        3,
-        false,
-        "last"
-      );
+        cy.checkMessageResponse(
+          "ASSISTANT",
+          "Set the mortality property of all Human instances to true.",
+          3,
+          false,
+          "last"
+        );
+      });
     });
   });
 });
