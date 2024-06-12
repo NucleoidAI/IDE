29997e75ffdfe647879162f152b3c26e5bf8304d
Ebubekir Yılmaz <ebu1205ed@gmail.com>
2024-06-12 10:33:47 +0300

    Wrap describes in Query Page

--- cypress/e2e/query-page.spec.cy.js
+++ cypress/e2e/query-page.spec.cy.js
@@ -1,179 +1,181 @@
-describe("Terminal Mode", () => {
-  beforeEach(() => {
-    cy.setup("IDE", "SEED", "LOCAL");
-    cy.fixture("PROJECTS/LOCAL/project.json").as("project");
-    cy.wrap("3450f289-0fc5-45e9-9a4a-606c0a63cdfe").as("projectId");
+describe("Query Page", () => {
+  describe("Terminal Mode", () => {
+    beforeEach(() => {
+      cy.setup("IDE", "SEED", "LOCAL");
+      cy.fixture("PROJECTS/LOCAL/project.json").as("project");
+      cy.wrap("3450f289-0fc5-45e9-9a4a-606c0a63cdfe").as("projectId");
 
-    cy.get("@projectId").then((projectId) => {
-      cy.visit(`/${projectId}/api?mode=local`);
+      cy.get("@projectId").then((projectId) => {
+        cy.visit(`/${projectId}/api?mode=local`);
+      });
+
+      cy.waitEvent("CONTAINER_LOADED");
+      cy.get("@projectId").then((projectId) => {
+        cy.visit(`/${projectId}/query?mode=terminal`);
+      });
     });
 
-    cy.waitEvent("CONTAINER_LOADED");
-    cy.get("@projectId").then((projectId) => {
-      cy.visit(`/${projectId}/query?mode=terminal`);
+    it("displays object tree when result is an object", () => {
+      cy.intercept("POST", "http://localhost:8448", {
+        fixture: "Query/query.object.json",
+      }).as("objectQuery");
+
+      const value = "return { id: 1 }";
+      cy.typeEditor(value);
+      cy.getBySel("query-button").click();
+
+      cy.wait("@objectQuery");
+
+      cy.getBySel("query-result-widget").find("[data-cy=time]").should("exist");
+      cy.getBySel("query-result-widget")
+        .find("[data-cy=done-icon]")
+        .should("exist");
+
+      cy.getBySel("query-result-widget")
+        .find('[data-cy="object"]')
+        .should("have.text", "{id:1}");
+    });
+    it("display object with value when result is a value", () => {
+      cy.intercept("POST", "http://localhost:8448", {
+        fixture: "Query/query.json",
+      }).as("valueQuery");
+
+      cy.typeEditor("a = 1");
+      cy.getBySel("query-button").click();
+
+      cy.wait("@valueQuery");
+
+      cy.getBySel("query-result-widget").find("[data-cy=time]").should("exist");
+      cy.getBySel("query-result-widget")
+        .find("[data-cy=done-icon]")
+        .should("exist");
+      cy.getBySel("query-result-widget")
+        .find('[data-cy="value"]')
+        .should("have.text", "{value:1}");
+    });
+    it("display success icon only when result is empty", function () {
+      cy.intercept("POST", "http://localhost:8448", {
+        fixture: "Query/query.empty.json",
+      }).as("emptyQuery");
+
+      const value = "class User {}";
+      cy.typeEditor(value);
+      cy.getBySel("query-button").click();
+
+      cy.wait("@emptyQuery");
+
+      cy.getBySel("query-result-widget").find("[data-cy=time]").should("exist");
+      cy.getBySel("query-result-widget")
+        .find("[data-cy=done-icon]")
+        .should("exist");
+    });
+    it("display array tree and data grid when result is an array", () => {
+      cy.intercept("POST", "http://localhost:8448", {
+        fixture: "Query/query.array.json",
+      }).as("arrayQuery");
+
+      const value = "return [{id:'5f5b3b4b-1b3b-4b3b-8b3b-3b3b3b3b3b3b'}]";
+      cy.typeEditor(value);
+      cy.getBySel("query-button").click();
+
+      cy.wait("@arrayQuery");
+
+      cy.getBySel("query-result-widget")
+        .find('[data-cy="array"]')
+        .should("have.text", '[0:{id:"5f5b3b4b-1b3b-4b3b-8b3b-3b3b3b3b3b3b"}]');
+
+      cy.getBySel("query-result-widget").find("[data-cy=time]").should("exist");
+      cy.getBySel("query-result-widget")
+        .find("[data-cy=done-icon]")
+        .should("exist");
+
+      cy.getBySel("json-switch").click();
     });
   });
 
-  it("displays object tree when result is an object", () => {
-    cy.intercept("POST", "http://localhost:8448", {
-      fixture: "Query/query.object.json",
-    }).as("objectQuery");
+  describe("Local Mode", () => {
+    beforeEach(() => {
+      cy.setup("IDE", "SEED", "LOCAL");
+      cy.fixture("PROJECTS/LOCAL/project.json").as("project");
+      cy.wrap("3450f289-0fc5-45e9-9a4a-606c0a63cdfe").as("projectId");
 
-    const value = "return { id: 1 }";
-    cy.typeEditor(value);
-    cy.getBySel("query-button").click();
+      cy.get("@projectId").then((projectId) => {
+        cy.visit(`/${projectId}/api?mode=local`);
+      });
 
-    cy.wait("@objectQuery");
+      cy.waitEvent("CONTAINER_LOADED");
 
-    cy.getBySel("query-result-widget").find("[data-cy=time]").should("exist");
-    cy.getBySel("query-result-widget")
-      .find("[data-cy=done-icon]")
-      .should("exist");
+      cy.runSandbox();
 
-    cy.getBySel("query-result-widget")
-      .find('[data-cy="object"]')
-      .should("have.text", "{id:1}");
-  });
-  it("display object with value when result is a value", () => {
-    cy.intercept("POST", "http://localhost:8448", {
-      fixture: "Query/query.json",
-    }).as("valueQuery");
-
-    cy.typeEditor("a = 1");
-    cy.getBySel("query-button").click();
-
-    cy.wait("@valueQuery");
-
-    cy.getBySel("query-result-widget").find("[data-cy=time]").should("exist");
-    cy.getBySel("query-result-widget")
-      .find("[data-cy=done-icon]")
-      .should("exist");
-    cy.getBySel("query-result-widget")
-      .find('[data-cy="value"]')
-      .should("have.text", "{value:1}");
-  });
-  it("display success icon only when result is empty", function () {
-    cy.intercept("POST", "http://localhost:8448", {
-      fixture: "Query/query.empty.json",
-    }).as("emptyQuery");
-
-    const value = "class User {}";
-    cy.typeEditor(value);
-    cy.getBySel("query-button").click();
-
-    cy.wait("@emptyQuery");
-
-    cy.getBySel("query-result-widget").find("[data-cy=time]").should("exist");
-    cy.getBySel("query-result-widget")
-      .find("[data-cy=done-icon]")
-      .should("exist");
-  });
-  it("display array tree and data grid when result is an array", () => {
-    cy.intercept("POST", "http://localhost:8448", {
-      fixture: "Query/query.array.json",
-    }).as("arrayQuery");
-
-    const value = "return [{id:'5f5b3b4b-1b3b-4b3b-8b3b-3b3b3b3b3b3b'}]";
-    cy.typeEditor(value);
-    cy.getBySel("query-button").click();
-
-    cy.wait("@arrayQuery");
-
-    cy.getBySel("query-result-widget")
-      .find('[data-cy="array"]')
-      .should("have.text", '[0:{id:"5f5b3b4b-1b3b-4b3b-8b3b-3b3b3b3b3b3b"}]');
-
-    cy.getBySel("query-result-widget").find("[data-cy=time]").should("exist");
-    cy.getBySel("query-result-widget")
-      .find("[data-cy=done-icon]")
-      .should("exist");
-
-    cy.getBySel("json-switch").click();
-  });
-});
-
-describe("Local Mode", () => {
-  beforeEach(() => {
-    cy.setup("IDE", "SEED", "LOCAL");
-    cy.fixture("PROJECTS/LOCAL/project.json").as("project");
-    cy.wrap("3450f289-0fc5-45e9-9a4a-606c0a63cdfe").as("projectId");
-
-    cy.get("@projectId").then((projectId) => {
-      cy.visit(`/${projectId}/api?mode=local`);
+      cy.getBySel("menu-Query").click();
     });
 
-    cy.waitEvent("CONTAINER_LOADED");
+    it("displays object tree when result is an object", () => {
+      cy.intercept("POST", "http://localhost:3000", {
+        fixture: "Query/query.object.json",
+      }).as("objectQuery");
 
-    cy.runSandbox();
+      const value = "return { id: 1 }";
+      cy.typeEditor(value);
+      cy.getBySel("query-button").click();
 
-    cy.getBySel("menu-Query").click();
-  });
+      cy.getBySel("query-result-widget").find("[data-cy=time]").should("exist");
+      cy.getBySel("query-result-widget")
+        .find("[data-cy=done-icon]")
+        .should("exist");
+      cy.getBySel("query-result-widget")
+        .find('[data-cy="object"]')
+        .should("have.text", "{id:1}");
+    });
+    it("display object with value when result is a value", () => {
+      cy.intercept("POST", "http://localhost:3000", {
+        fixture: "Query/query.json",
+      }).as("valueQuery");
 
-  it("displays object tree when result is an object", () => {
-    cy.intercept("POST", "http://localhost:3000", {
-      fixture: "Query/query.object.json",
-    }).as("objectQuery");
+      cy.typeEditor("a = 1");
+      cy.getBySel("query-button").click();
 
-    const value = "return { id: 1 }";
-    cy.typeEditor(value);
-    cy.getBySel("query-button").click();
+      cy.getBySel("query-result-widget").find("[data-cy=time]").should("exist");
+      cy.getBySel("query-result-widget")
+        .find("[data-cy=done-icon]")
+        .should("exist");
+      cy.getBySel("query-result-widget")
+        .find('[data-cy="value"]')
+        .should("have.text", "{value:1}");
+    });
+    it("display success icon only when result is empty", function () {
+      cy.intercept("POST", "http://localhost:3000", {
+        fixture: "Query/query.empty.json",
+      }).as("emptyQuery");
 
-    cy.getBySel("query-result-widget").find("[data-cy=time]").should("exist");
-    cy.getBySel("query-result-widget")
-      .find("[data-cy=done-icon]")
-      .should("exist");
-    cy.getBySel("query-result-widget")
-      .find('[data-cy="object"]')
-      .should("have.text", "{id:1}");
-  });
-  it("display object with value when result is a value", () => {
-    cy.intercept("POST", "http://localhost:3000", {
-      fixture: "Query/query.json",
-    }).as("valueQuery");
+      const value = "class User {}";
+      cy.typeEditor(value);
+      cy.getBySel("query-button").click();
 
-    cy.typeEditor("a = 1");
-    cy.getBySel("query-button").click();
+      cy.getBySel("query-result-widget").find("[data-cy=time]").should("exist");
+      cy.getBySel("query-result-widget")
+        .find("[data-cy=done-icon]")
+        .should("exist");
+    });
+    it("display array tree and data grid when result is an array", () => {
+      cy.intercept("POST", "http://localhost:3000", {
+        fixture: "Query/query.array.json",
+      }).as("arrayQuery");
 
-    cy.getBySel("query-result-widget").find("[data-cy=time]").should("exist");
-    cy.getBySel("query-result-widget")
-      .find("[data-cy=done-icon]")
-      .should("exist");
-    cy.getBySel("query-result-widget")
-      .find('[data-cy="value"]')
-      .should("have.text", "{value:1}");
-  });
-  it("display success icon only when result is empty", function () {
-    cy.intercept("POST", "http://localhost:3000", {
-      fixture: "Query/query.empty.json",
-    }).as("emptyQuery");
+      const value = "return [{id:'5f5b3b4b-1b3b-4b3b-8b3b-3b3b3b3b3b3b'}]";
+      cy.typeEditor(value);
+      cy.getBySel("query-button").click();
 
-    const value = "class User {}";
-    cy.typeEditor(value);
-    cy.getBySel("query-button").click();
+      cy.getBySel("query-result-widget")
+        .find('[data-cy="array"]')
+        .should("have.text", '[0:{id:"5f5b3b4b-1b3b-4b3b-8b3b-3b3b3b3b3b3b"}]');
 
-    cy.getBySel("query-result-widget").find("[data-cy=time]").should("exist");
-    cy.getBySel("query-result-widget")
-      .find("[data-cy=done-icon]")
-      .should("exist");
-  });
-  it("display array tree and data grid when result is an array", () => {
-    cy.intercept("POST", "http://localhost:3000", {
-      fixture: "Query/query.array.json",
-    }).as("arrayQuery");
+      cy.getBySel("query-result-widget").find("[data-cy=time]").should("exist");
+      cy.getBySel("query-result-widget")
+        .find("[data-cy=done-icon]")
+        .should("exist");
 
-    const value = "return [{id:'5f5b3b4b-1b3b-4b3b-8b3b-3b3b3b3b3b3b'}]";
-    cy.typeEditor(value);
-    cy.getBySel("query-button").click();
-
-    cy.getBySel("query-result-widget")
-      .find('[data-cy="array"]')
-      .should("have.text", '[0:{id:"5f5b3b4b-1b3b-4b3b-8b3b-3b3b3b3b3b3b"}]');
-
-    cy.getBySel("query-result-widget").find("[data-cy=time]").should("exist");
-    cy.getBySel("query-result-widget")
-      .find("[data-cy=done-icon]")
-      .should("exist");
-
-    cy.getBySel("json-switch").click();
+      cy.getBySel("json-switch").click();
+    });
   });
 });
